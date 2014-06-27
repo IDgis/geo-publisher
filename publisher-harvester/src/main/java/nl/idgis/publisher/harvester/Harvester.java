271f8e54b1c651f4718843425da2ea3f9c64f61a
copierrj <reijer.copier@idgis.nl>
2014-06-27 17:37:38 +0200

    monitoring output enabled for harvester

--- publisher-harvester/src/main/java/nl/idgis/publisher/harvester/Harvester.java
+++ publisher-harvester/src/main/java/nl/idgis/publisher/harvester/Harvester.java
@@ -5,24 +5,33 @@ import java.util.concurrent.TimeUnit;
 import com.typesafe.config.Config;
 
 import nl.idgis.publisher.harvester.messages.Harvest;
+import nl.idgis.publisher.monitor.messages.GetTree;
+import nl.idgis.publisher.monitor.messages.Tree;
 import nl.idgis.publisher.utils.Boot;
 import nl.idgis.publisher.utils.Initiator;
-
 import scala.concurrent.duration.Duration;
-
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
-public class Harvester extends UntypedActor {
+public class Harvester extends UntypedActor {	
 	
-	private final Config config;	
+	private final Config config;
+	private final ActorRef monitor;
+	private final LoggingAdapter log;
 
-	public Harvester(Config config) {
-		this.config = config;		
+	public Harvester(Config config, ActorRef monitor) {
+		this.config = config;
+		this.monitor = monitor;
+		
+		log = Logging.getLogger(getContext().system(), this);
 	}
 	
-	public static Props props(Config config) {
-		return Props.create(Harvester.class, config);
+	public static Props props(Config config, ActorRef monitor) {
+		return Props.create(Harvester.class, config, monitor);
 	}
 
 	@Override
@@ -34,15 +43,23 @@ public class Harvester extends UntypedActor {
 				Initiator.props("../server/client*/harvester",
 						Duration.create(10, TimeUnit.SECONDS), new Harvest()),
 				"initiator");
+		
+		ActorSystem system = getContext().system();
+		system.scheduler().schedule(Duration.Zero(), Duration.create(10, TimeUnit.SECONDS), 
+				monitor, new GetTree(), system.dispatcher(), getSelf());
 	}
 
 	@Override
 	public void onReceive(Object msg) throws Exception {
-		unhandled(msg);		
+		if(msg instanceof Tree) {
+			log.debug(msg.toString());
+		} else {
+			unhandled(msg);
+		}
 	}
 	
 	public static void main(String[] args) {
 		Boot boot = Boot.init("harvester");
-		boot.startPublisher(Harvester.props(boot.getConfig()));
+		boot.startPublisher(Harvester.props(boot.getConfig(), boot.getMonitor()));
 	}
 }
