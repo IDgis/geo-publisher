e0005e72c1afc40e7f40f769065953de04b16529
copierrj <reijer.copier@idgis.nl>
2014-06-27 09:05:34 +0200

    error handling and logging improved

--- publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessageListener.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessageListener.java
@@ -17,38 +17,47 @@ public abstract class MessageListener extends UntypedActor {
 			this);
 	
 	protected class LocalActorFactory {
-		
+
 		String name;
-		
+
 		private LocalActorFactory() {
-			
+
 		}
-		
+
 		public LocalActorFactory newActor(String name) {
 			this.name = name;
-			
+
 			return this;
 		}
-		
+
 		public LocalActorFactory existingActor(String name, ActorRef actorRef) {
 			log.debug("existing actor added: " + name);
-			
+
 			this.name = name;
-			
+
 			localActors.put(name, new LocalActor(actorRef));
-			
+
 			return this;
 		}
-		
+
 		public ActorRef getRemoteRef(String targetName) {
+			if(name == null) {
+				throw new IllegalStateException("No actor");
+			}
+			
 			log.debug("creating remote ref: " + name + " -> " + targetName);
-			
+
 			log.debug(getSender().toString());
-			
-			return getContext().actorOf(MessagePackager.props(targetName, name, getSender()));
+
+			return getContext().actorOf(
+					MessagePackager.props(targetName, name, getSender()));
 		}
-		
+
 		public LocalActorFactory actorOf(Props props) {
+			if(name == null) {
+				throw new IllegalStateException("No actor");
+			}
+			
 			log.debug("new actor added: " + name);
 			
 			localActors.put(name, new LocalActor(getContext().actorOf(props, name)));
@@ -56,57 +65,62 @@ public abstract class MessageListener extends UntypedActor {
 			return this;
 		}
 	}
-	
+
 	private class LocalActor {
-		
+
 		final ActorRef actorRef;
 		final Map<String, ActorRef> packagers;
-		
+
 		LocalActor(ActorRef actorRef) {
 			this.actorRef = actorRef;
 			this.packagers = new HashMap<String, ActorRef>();
-		} 
+		}
 	}
 
-	private Map<String, LocalActor> localActors = new HashMap<String, LocalActor>();	
-	
+	private Map<String, LocalActor> localActors = new HashMap<String, LocalActor>();
+
 	protected abstract void connected(LocalActorFactory actorFactory);
-	
+
 	protected abstract void connectionClosed();
 
 	@Override
 	public final void onReceive(Object msg) throws Exception {
 		if (msg instanceof Connected) {
 			log.debug("connected");
-			
+
 			connected(new LocalActorFactory());
 		} else if (msg instanceof ConnectionClosed) {
 			log.debug("disconnected");
-			
+
 			connectionClosed();
 			getContext().stop(getSelf());
 		} else if (msg instanceof Message) {
-			log.debug("dispatching message");	
-			
-			Message message = (Message)msg;
-			
+			log.debug("dispatching message");
+
+			Message message = (Message) msg;
+
 			String targetName = message.getTargetName();
 			LocalActor localActor = localActors.get(targetName);
-			
-			if(localActor == null) {
-				throw new IllegalStateException("Local actor unknown: " + targetName);
+
+			if (localActor == null) {
+				throw new IllegalStateException("Local actor unknown: "
+						+ targetName);
 			}
 
 			ActorRef returnPackager;
-			String sourceName = message.getSourceName();			
-			if(localActor.packagers.containsKey(sourceName)) {
+			String sourceName = message.getSourceName();
+			if (localActor.packagers.containsKey(sourceName)) {
 				returnPackager = localActor.packagers.get(sourceName);
 			} else {
-				returnPackager = getContext().actorOf(MessagePackager.props(sourceName, targetName, getSender()));
+				log.debug("creating remote ref: " + targetName + " -> " + sourceName);
+				
+				returnPackager = getContext().actorOf(
+						MessagePackager.props(sourceName, targetName,
+								getSender()));
 				localActor.packagers.put(sourceName, returnPackager);
 			}
-			
-			localActor.actorRef.tell(message.getContent(), returnPackager);			
+
+			localActor.actorRef.tell(message.getContent(), returnPackager);
 		} else {
 			unhandled(msg);
 		}
