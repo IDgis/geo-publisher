ff2c8de478536c1c08901321b90f6fd5640233de
copierrj <reijer.copier@idgis.nl>
2014-07-21 12:19:34 +0200

    DatabaseTest added

---
+++ publisher-database/src/test/java/nl/idgis/publisher/database/DatabaseTest.java
@@ -0,0 +1,45 @@
+package nl.idgis.publisher.database;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.mysema.query.Tuple;
+import com.mysema.query.sql.PostgresTemplates;
+import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.sql.SQLTemplates;
+
+import static nl.idgis.publisher.database.QVersion.version;
+
+public class DatabaseTest {
+
+	@Test
+	public void testVersion() throws Exception {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("target/db.properties"));
+		
+		Class.forName((String)properties.get("database.driver"));
+		
+		String url = (String)properties.get("database.url");
+		String user = (String)properties.get("database.user");
+		String password = (String)properties.get("database.password");
+		Connection connection = DriverManager.getConnection(url, user, password);
+		
+		SQLTemplates dialect = new PostgresTemplates() {{
+			setPrintSchema(true);
+		}};
+		SQLQuery query = new SQLQuery(connection, dialect);
+		for(Tuple tuple : query.from(version).list(version.id, version.createTime)) {
+			Integer id = tuple.get(version.id);
+			Timestamp createTime = tuple.get(version.createTime);
+			
+			System.out.println("id: " + id + " createTime: " + createTime);
+		}
+		
+		connection.close();
+	}
+}
