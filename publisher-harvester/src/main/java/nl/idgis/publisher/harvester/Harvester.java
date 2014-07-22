63456271290a86fabbbed1567f0709a7399e04ab
copierrj <reijer.copier@idgis.nl>
2014-07-22 12:42:45 +0200

    harvesting scheduler (re)implemented

--- publisher-harvester/src/main/java/nl/idgis/publisher/harvester/Harvester.java
+++ publisher-harvester/src/main/java/nl/idgis/publisher/harvester/Harvester.java
@@ -1,9 +1,15 @@
 package nl.idgis.publisher.harvester;
 
+import java.util.concurrent.TimeUnit;
+
+import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
+import nl.idgis.publisher.harvester.messages.Harvest;
 import nl.idgis.publisher.harvester.messages.ProviderConnected;
 import nl.idgis.publisher.harvester.server.Server;
 import nl.idgis.publisher.utils.ConfigUtils;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.Terminated;
 import akka.actor.UntypedActor;
@@ -40,6 +46,9 @@ public class Harvester extends UntypedActor {
 		getContext().actorOf(Server.props(getSelf(), port, sslConfig), "server");
 		
 		providerClients = HashBiMap.create();
+		
+		FiniteDuration interval = Duration.create(10, TimeUnit.SECONDS);
+		getContext().system().scheduler().schedule(Duration.Zero(), interval, getSelf(), new Harvest(), getContext().dispatcher(), getSelf());
 	}
 
 	@Override
@@ -54,6 +63,11 @@ public class Harvester extends UntypedActor {
 			String providerName = providerClients.inverse().remove(((Terminated) msg).getActor());
 			if(providerName != null) {
 				log.debug("Connection lost, provider: " + providerName);
+			}
+		} else if (msg instanceof Harvest) {
+			log.debug("starting harvesting");
+			for(ActorRef providerClient : providerClients.values()) {
+				providerClient.tell(msg, getSelf());
 			}
 		} else {
 			unhandled(msg);
