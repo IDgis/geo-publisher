71411199605f533f973238e9aa2f3ef4848623de
Erik Orbons <erik.orbons@idgis.nl>
2014-08-04 17:22:58 +0200

    Dataset form can be submitted and validated.

--- publisher-web/app/controllers/Datasets.java
+++ publisher-web/app/controllers/Datasets.java
@@ -3,11 +3,13 @@ package controllers;
 import static models.Domain.from;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.ArrayList;
 
 import models.Domain.Function;
 import models.Domain.Function2;
+import models.Domain.Function3;
 import nl.idgis.publisher.domain.query.ListColumns;
 import nl.idgis.publisher.domain.query.ListDatasets;
 import nl.idgis.publisher.domain.response.Page;
@@ -17,6 +19,7 @@ import nl.idgis.publisher.domain.web.DataSource;
 import nl.idgis.publisher.domain.web.Dataset;
 import nl.idgis.publisher.domain.web.PutDataset;
 import nl.idgis.publisher.domain.web.SourceDataset;
+import play.Logger;
 import play.Play;
 import play.libs.Akka;
 import play.libs.F.Promise;
@@ -86,8 +89,7 @@ public class Datasets extends Controller {
 		return list(1);
 	}
 	
-	public static Promise<Result> createForm () {
-		
+	private static Promise<Result> renderCreateForm (final Form<DatasetForm> datasetForm) {
 		final ActorSelection database = Akka.system().actorSelection (databaseRef);
 		
 		return from(database)
@@ -96,15 +98,42 @@ public class Datasets extends Controller {
 			.execute(new Function2<Page<DataSource>, Page<Category>, Result>() {
 				@Override
 				public Result apply(Page<DataSource> dataSources, Page<Category> categories) throws Throwable {
-					final Form<DatasetForm> datasetForm = Form.form (DatasetForm.class);
-					
 					return ok (form.render (dataSources, categories, datasetForm));
 				}
 			});
 	}
 	
+	public static Promise<Result> createForm () {
+
+		final Form<DatasetForm> datasetForm = Form.form (DatasetForm.class);
+		
+		return renderCreateForm (datasetForm);
+	}
+	
 	public static Promise<Result> submitCreate () {
-		return Promise.pure ((Result) ok ());
+		final ActorSelection database = Akka.system().actorSelection (databaseRef);
+		final Form<DatasetForm> datasetForm = Form.form (DatasetForm.class).bindFromRequest ();
+		
+		if (datasetForm.hasErrors ()) {
+			return renderCreateForm (datasetForm);
+		}
+		
+		final DatasetForm dataset = datasetForm.get ();
+		
+		return from (database)
+				.get (DataSource.class, dataset.getDataSourceId ())
+				.get (Category.class, dataset.getCategoryId ())
+				.get (SourceDataset.class, dataset.getSourceDatasetId ())
+				.executeFlat (new Function3<DataSource, Category, SourceDataset, Promise<Result>> () {
+					@Override
+					public Promise<Result> apply (final DataSource dataSource, final Category category, final SourceDataset sourceDataset) throws Throwable {
+						Logger.debug ("dataSource: " + dataSource);
+						Logger.debug ("category: " + category);
+						Logger.debug ("sourceDataset: " + sourceDataset);
+
+						return renderCreateForm (datasetForm);
+					}
+				});
 	}
 	
 	public static Promise<Result> editForm (final String datasetId) {
@@ -156,7 +185,7 @@ public class Datasets extends Controller {
 	
 					@Override
 					public Result apply(List<Column> c) throws Throwable {
-						return ok(columns.render(c));
+						return ok(columns.render(c, null));
 					}
 				});
 	}
@@ -176,7 +205,7 @@ public class Datasets extends Controller {
 		@Constraints.Required
 		private String sourceDatasetId;
 		
-		private Set<String> columns;
+		private Map<String, String> columns;
 		
 		public String getName() {
 			return name;
@@ -210,11 +239,11 @@ public class Datasets extends Controller {
 			this.sourceDatasetId = sourceDatasetId;
 		}
 		
-		public Set<String> getColumns () {
+		public Map<String, String> getColumns () {
 			return columns;
 		}
 		
-		public void setColumns (final Set<String> columns) {
+		public void setColumns (final Map<String, String> columns) {
 			this.columns = columns;
 		}
 	}
