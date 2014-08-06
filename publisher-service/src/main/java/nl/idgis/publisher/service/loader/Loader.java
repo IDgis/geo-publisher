1c8118bf50c92779f610e98d5d6f56bf18e2d49e
copierrj <reijer.copier@idgis.nl>
2014-08-06 12:21:25 +0200

    message handling moved to separate methods

--- publisher-service/src/main/java/nl/idgis/publisher/service/loader/Loader.java
+++ publisher-service/src/main/java/nl/idgis/publisher/service/loader/Loader.java
@@ -14,7 +14,6 @@ import nl.idgis.publisher.domain.service.Column;
 import nl.idgis.publisher.harvester.messages.GetDataSource;
 import nl.idgis.publisher.harvester.messages.NotConnected;
 import nl.idgis.publisher.harvester.sources.messages.GetDataset;
-
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
@@ -47,82 +46,84 @@ public class Loader extends UntypedActor {
 	@Override
 	public void onReceive(Object msg) throws Exception {
 		if(msg instanceof ImportJob) {
-			final ImportJob importJob = (ImportJob)msg;
-			
-			log.debug("data import requested: " + importJob);
-			
-			final String dataSourceId = importJob.getDataSourceId();
-			Patterns.ask(harvester, new GetDataSource(dataSourceId), 15000)
-				.onSuccess(new OnSuccess<Object>() {
-
-					@Override
-					public void onSuccess(Object msg) throws Throwable {
-						if(msg instanceof NotConnected) {
-							log.warning("not connected: " + dataSourceId);
-							
-							ImportLogLine logLine = new ImportLogLine(GenericEvent.FINISHED, importJob.getDatasetId());
-							database.tell(new StoreLog(logLine), getSelf());
-						} else {
-							final ActorRef dataSource = (ActorRef)msg;
-							
-							log.debug("dataSource received");
-							
-							ImportLogLine logLine = new ImportLogLine(GenericEvent.STARTED, importJob.getDatasetId());
-							Patterns.ask(database, new StoreLog(logLine), 15000)
-								.onSuccess(new OnSuccess<Object>() {
-
-									@Override
-									public void onSuccess(Object msg) throws Throwable {
-										log.debug("started logline written");
-										
-										Patterns.ask(geometryDatabase, new StartTransaction(), 15000)
-										.onSuccess(new OnSuccess<Object>() {
-
-											@Override
-											public void onSuccess(Object msg) throws Throwable {
-												TransactionCreated tc = (TransactionCreated)msg;
-												log.debug("database transaction created");
-												
-												final ActorRef transaction = tc.getActor();
-												
-												CreateTable ct = new CreateTable(
-														importJob.getDatasetId(),  
-														importJob.getColumns());						
-												
-												Patterns.ask(transaction, ct, 15000)
-													.onSuccess(new OnSuccess<Object>() {
-
-														@Override
-														public void onSuccess(Object msg) throws Throwable {
-															log.debug("table created");
-															
-															List<String> columnNames = new ArrayList<>();
-															for(Column column : importJob.getColumns()) {
-																columnNames.add(column.getName());
-															}
-															
-															dataSource.tell(
-																	new GetDataset(
-																			importJob.getSourceDatasetId(), 
-																			columnNames, 
-																			LoaderSession.props(
-																					importJob, 
-																					transaction, 
-																					database)), getSelf());
-														}
-														
-													}, getContext().dispatcher());
-											}					
-										}, getContext().dispatcher());
-									}
-									
-								}, getContext().dispatcher());
-						}
-					}
-					
-				}, getContext().dispatcher());
+			handleImportJob((ImportJob)msg);
 		} else {
 			unhandled(msg);
 		}
+	}
+
+	private void handleImportJob(final ImportJob importJob) {
+		log.debug("data import requested: " + importJob);
+		
+		final String dataSourceId = importJob.getDataSourceId();
+		Patterns.ask(harvester, new GetDataSource(dataSourceId), 15000)
+			.onSuccess(new OnSuccess<Object>() {
+
+				@Override
+				public void onSuccess(Object msg) throws Throwable {
+					if(msg instanceof NotConnected) {
+						log.warning("not connected: " + dataSourceId);
+						
+						ImportLogLine logLine = new ImportLogLine(GenericEvent.FINISHED, importJob.getDatasetId());
+						database.tell(new StoreLog(logLine), getSelf());
+					} else {
+						final ActorRef dataSource = (ActorRef)msg;
+						
+						log.debug("dataSource received");
+						
+						ImportLogLine logLine = new ImportLogLine(GenericEvent.STARTED, importJob.getDatasetId());
+						Patterns.ask(database, new StoreLog(logLine), 15000)
+							.onSuccess(new OnSuccess<Object>() {
+
+								@Override
+								public void onSuccess(Object msg) throws Throwable {
+									log.debug("started logline written");
+									
+									Patterns.ask(geometryDatabase, new StartTransaction(), 15000)
+									.onSuccess(new OnSuccess<Object>() {
+
+										@Override
+										public void onSuccess(Object msg) throws Throwable {
+											TransactionCreated tc = (TransactionCreated)msg;
+											log.debug("database transaction created");
+											
+											final ActorRef transaction = tc.getActor();
+											
+											CreateTable ct = new CreateTable(
+													importJob.getDatasetId(),  
+													importJob.getColumns());						
+											
+											Patterns.ask(transaction, ct, 15000)
+												.onSuccess(new OnSuccess<Object>() {
+
+													@Override
+													public void onSuccess(Object msg) throws Throwable {
+														log.debug("table created");
+														
+														List<String> columnNames = new ArrayList<>();
+														for(Column column : importJob.getColumns()) {
+															columnNames.add(column.getName());
+														}
+														
+														dataSource.tell(
+																new GetDataset(
+																		importJob.getSourceDatasetId(), 
+																		columnNames, 
+																		LoaderSession.props(
+																				importJob, 
+																				transaction, 
+																				database)), getSelf());
+													}
+													
+												}, getContext().dispatcher());
+										}					
+									}, getContext().dispatcher());
+								}
+								
+							}, getContext().dispatcher());
+					}
+				}
+				
+			}, getContext().dispatcher());
 	}	
 }
