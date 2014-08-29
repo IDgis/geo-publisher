efe66d8f6e3942ad8f26616104b05684c529b383
copierrj <reijer.copier@idgis.nl>
2014-08-29 10:17:23 +0200

    initiator test implemented

---
+++ publisher-service/src/test/java/nl/idgis/publisher/job/InitiatorTest.java
@@ -0,0 +1,91 @@
+package nl.idgis.publisher.job;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
+import nl.idgis.publisher.database.AbstractDatabaseTest;
+import nl.idgis.publisher.database.messages.CreateDataset;
+import nl.idgis.publisher.database.messages.CreateHarvestJob;
+import nl.idgis.publisher.database.messages.CreateImportJob;
+import nl.idgis.publisher.database.messages.CreateServiceJob;
+import nl.idgis.publisher.database.messages.HarvestJobInfo;
+import nl.idgis.publisher.database.messages.ImportJobInfo;
+import nl.idgis.publisher.database.messages.JobInfo;
+import nl.idgis.publisher.database.messages.RegisterSourceDataset;
+import nl.idgis.publisher.database.messages.ServiceJobInfo;
+import nl.idgis.publisher.domain.service.Dataset;
+import nl.idgis.publisher.domain.service.Table;
+
+public class InitiatorTest extends AbstractDatabaseTest {
+	
+	static class GetLastReceivedJob {
+		
+	}
+	
+	static class JobReceiver extends UntypedActor {
+		
+		JobInfo lastJob = null;
+
+		@Override
+		public void onReceive(Object msg) throws Exception { 
+			if(msg instanceof JobInfo) {
+				lastJob = (JobInfo)msg;
+			} else if(msg instanceof GetLastReceivedJob) {
+				if(lastJob != null) {
+					getSender().tell(lastJob, getSelf());
+				}
+			}
+		}
+		
+	}
+	
+	ActorRef initiator;
+	
+	ActorRef harvester;
+	ActorRef loader;
+	ActorRef service;
+	
+	@Before
+	public void databaseContent() throws Exception {
+		insertDataSource();
+		
+		Dataset testDataset = createTestDataset();
+		ask(database, new RegisterSourceDataset("testDataSource", testDataset));
+		
+		Table testTable = testDataset.getTable();
+		ask(database, new CreateDataset("testDataset", "My Test Dataset", testDataset.getId(), testTable.getColumns(), ""));
+	}
+	
+	@Before	
+	public void actors() throws Exception {
+		Props jobReceiverProps = Props.create(JobReceiver.class);
+		
+		harvester = actorOf(jobReceiverProps);
+		loader = actorOf(jobReceiverProps);
+		service = actorOf(jobReceiverProps);
+		
+		initiator = actorOf(Initiator.props(database, harvester, loader, service));
+	}
+	
+	@Test
+	public void testHarvestJob() throws Exception {
+		ask(database, new CreateHarvestJob("testDataSource"));
+		ask(harvester, new GetLastReceivedJob(), HarvestJobInfo.class);
+	}
+	
+	@Test
+	public void testImportJob() throws Exception {
+		ask(database, new CreateImportJob("testDataset"));		
+		ask(loader, new GetLastReceivedJob(), ImportJobInfo.class);
+	}
+	
+	@Test
+	public void testServiceJob() throws Exception {
+		ask(database, new CreateServiceJob("testDataset"));		
+		ask(service, new GetLastReceivedJob(), ServiceJobInfo.class);
+	}
+}
