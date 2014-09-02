9e22bf616c7e89dfe0647ba0e514980aea816ddd
Erik Orbons <erik.orbons@idgis.nl>
2014-09-02 10:07:31 +0200

    - Events are not bound to the datasets list when the list is empty. - Logging is updated when new errors are reported. - Reordered datasets routes.

--- publisher-web/app/assets/js/datasets/main.js
+++ publisher-web/app/assets/js/datasets/main.js
@@ -27,28 +27,30 @@ require ([
 	topic,
 	xhr
 ) {
-	on (dom.byId ('dataset-list'), '.js-dataset-refresh:click', function (e) {
-		e.preventDefault ();
-		e.stopPropagation ();
-
-		var datasetItem = query (this).closest ('.js-dataset-item'), 
-			datasetId = datasetItem.attr ('data-dataset-id')[0],
-			datasetName = datasetItem.attr ('data-dataset-name')[0];
+	if (dom.byId ('dataset-list')) {
+		on (dom.byId ('dataset-list'), '.js-dataset-refresh:click', function (e) {
+			e.preventDefault ();
+			e.stopPropagation ();
 	
-		domClass.add (query ('.js-icon', this)[0], 'rotating');
+			var datasetItem = query (this).closest ('.js-dataset-item'), 
+				datasetId = datasetItem.attr ('data-dataset-id')[0],
+				datasetName = datasetItem.attr ('data-dataset-name')[0];
 		
-		xhr.post (jsRoutes.controllers.Datasets.scheduleRefresh (datasetId).url, {
-			handleAs: 'json'
-		}).then (function (data) {
-			if (data.result && data.result == 'ok') {
-				topic.publish ('publisher/notification', 'success', 'Verversen van dataset ' + datasetName + ' ingepland.');
-			} else {
+			domClass.add (query ('.js-icon', this)[0], 'rotating');
+			
+			xhr.post (jsRoutes.controllers.Datasets.scheduleRefresh (datasetId).url, {
+				handleAs: 'json'
+			}).then (function (data) {
+				if (data.result && data.result == 'ok') {
+					topic.publish ('publisher/notification', 'success', 'Verversen van dataset ' + datasetName + ' ingepland.');
+				} else {
+					topic.publish ('publisher/notification', 'danger', 'Kan verversen van dataset ' + datasetName + ' niet inplannen.');
+				}
+			}, function () {
 				topic.publish ('publisher/notification', 'danger', 'Kan verversen van dataset ' + datasetName + ' niet inplannen.');
-			}
-		}, function () {
-			topic.publish ('publisher/notification', 'danger', 'Kan verversen van dataset ' + datasetName + ' niet inplannen.');
+			});
 		});
-	});
+	}
 	
 	// =========================================================================
 	// Jobs:
--- publisher-web/app/assets/js/logging/main.js
+++ publisher-web/app/assets/js/logging/main.js
@@ -1,3 +1,19 @@
-require ([], function () {
-	
+require ([
+    'dojo/dom',
+    'dojo/dom-class',
+    'dojo/topic'
+], function (
+	dom,
+	domClass,
+	topic
+) {
+	topic.subscribe ('publisher/issues', function (issues) {
+		if (issues.list.length > 0) {
+			var hintNode = dom.byId ('new-issues-hint');
+			console.log ('New issues');
+			if (hintNode) {
+				domClass.remove (hintNode, 'hidden');
+			}
+		}
+	});
 });
