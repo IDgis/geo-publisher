9ae7c2c498ee01ce6ed757e823a704625ff09aa4
copierrj <reijer.copier@idgis.nl>
2014-09-18 17:47:36 +0200

    missing serializable added

--- publisher-commons/src/main/java/nl/idgis/publisher/stream/messages/Start.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/stream/messages/Start.java
@@ -1,5 +1,9 @@
 package nl.idgis.publisher.stream.messages;
 
-public abstract class Start {
+import java.io.Serializable;
+
+public abstract class Start implements Serializable {
+
+	private static final long serialVersionUID = 3306258567192194024L;
 
 }
