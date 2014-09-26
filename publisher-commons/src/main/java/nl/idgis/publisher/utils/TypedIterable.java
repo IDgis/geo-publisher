2490f52bac5aa4a8bd75eb4ae126cadd95910e7d
copierrj <reijer.copier@idgis.nl>
2014-09-26 17:28:38 +0200

    TypedIterable.asCollection implemented

--- publisher-commons/src/main/java/nl/idgis/publisher/utils/TypedIterable.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/utils/TypedIterable.java
@@ -1,6 +1,8 @@
 package nl.idgis.publisher.utils;
 
 import java.io.Serializable;
+import java.util.AbstractCollection;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class TypedIterable<T> implements Iterable<T>, Serializable {
@@ -31,6 +33,34 @@ public class TypedIterable<T> implements Iterable<T>, Serializable {
 	@Override
 	public Iterator<T> iterator() {
 		return content.iterator();
+	}
+	
+	public Collection<T> asCollection() {
+		return new AbstractCollection<T>() {
+			
+			private Integer size;
+
+			@Override
+			public Iterator<T> iterator() {
+				return iterator();
+			}
+
+			@Override
+			public int size() {				
+				if(size == null) {
+					int size = 0;
+					
+					for(Iterator<T> i = iterator(); i.hasNext();) {
+						size++;
+					}
+					
+					this.size = size;
+				}
+				
+				return size;
+			}
+			
+		};
 	}
 
 	@Override
