10d1e21a7ba835f914d9b266c4edf263b71d6e8a
copierrj <reijer.copier@idgis.nl>
2014-11-03 17:41:53 +0100

    additional methods added to AsyncSQLQuery

--- publisher-database/src/main/java/nl/idgis/publisher/database/Async.java
+++ publisher-database/src/main/java/nl/idgis/publisher/database/Async.java
@@ -12,4 +12,9 @@ public interface Async {
 	Future<TypedList<Tuple>> list(Expression<?>... args);
 	<RT> Future<TypedList<RT>> list(Expression<RT> projection);
 	Future<TypedList<Tuple>> list(Object... args);
+	Future<Tuple> singleResult(Expression<?>... args);
+	<RT> Future<RT> singleResult(Expression<RT> projection);
+	Future<Tuple> singleResult(Object... args);
+	Future<Boolean> exists();
+	Future<Boolean> notExists();
 }
--- publisher-database/src/main/java/nl/idgis/publisher/database/AsyncSQLQuery.java
+++ publisher-database/src/main/java/nl/idgis/publisher/database/AsyncSQLQuery.java
@@ -1,21 +1,23 @@
 package nl.idgis.publisher.database;
 
+import java.util.Iterator;
+
 import nl.idgis.publisher.database.messages.PerformQuery;
 import nl.idgis.publisher.utils.TypedList;
-
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
-
 import akka.actor.ActorRef;
 import akka.dispatch.Mapper;
 import akka.pattern.Patterns;
 import akka.util.Timeout;
 
 import com.mysema.query.DefaultQueryMetadata;
+import com.mysema.query.NonUniqueResultException;
 import com.mysema.query.QueryMetadata;
 import com.mysema.query.Tuple;
 import com.mysema.query.support.Expressions;
 import com.mysema.query.types.Expression;
+import com.mysema.query.types.template.NumberTemplate;
 
 public class AsyncSQLQuery extends AbstractAsyncSQLQuery<AsyncSQLQuery> implements Async {
 	
@@ -74,6 +76,10 @@ public class AsyncSQLQuery extends AbstractAsyncSQLQuery<AsyncSQLQuery> implemen
 
 	@Override
 	public Future<TypedList<Tuple>> list(Object... args) {
+		return list(asExpressions(args));
+	}
+
+	private Expression<?>[] asExpressions(Object... args) {
 		Expression<?>[] exprArgs = new Expression<?>[args.length];
 		
 		int i = 0;
@@ -84,8 +90,66 @@ public class AsyncSQLQuery extends AbstractAsyncSQLQuery<AsyncSQLQuery> implemen
 				exprArgs[i++] = Expressions.constant(arg);
 			}
 		}
-		
-		return list(exprArgs);
+		return exprArgs;
+	}
+	
+	private <T> Future<T> singleResult(Future<TypedList<T>> listResult) {
+		return listResult.map(new Mapper<TypedList<T>, T>() {
+			
+			@Override
+			public T checkedApply(TypedList<T> list) {
+				Iterator<T> itr = list.iterator();
+				
+				if(itr.hasNext()) {
+					return null;
+				} else {
+					T retval = itr.next();
+					if(itr.hasNext()) {
+						throw new NonUniqueResultException();
+					} else {
+						return retval;
+					}
+				}
+			}
+		}, executionContext);
 	}
 
+	@Override
+	public Future<Tuple> singleResult(Expression<?>... args) {
+		return singleResult(list(args));
+	}
+
+	@Override
+	public <RT> Future<RT> singleResult(Expression<RT> projection) {
+		return singleResult(list(projection));
+	}
+
+	@Override
+	public Future<Tuple> singleResult(Object... args) {
+		return singleResult(asExpressions(args));
+	}
+
+	@Override
+	public Future<Boolean> exists() {
+		return limit(1).singleResult(NumberTemplate.ONE).map(new Mapper<Integer, Boolean>() {
+			
+			@Override
+			public Boolean apply(Integer i) {
+				return i != null;
+			}
+			
+		}, executionContext);
+	}
+
+	@Override
+	public Future<Boolean> notExists() { 
+		return exists().map(new Mapper<Boolean, Boolean>() {
+			
+			@Override
+			public Boolean apply(Boolean b) {
+				return !b;
+			}
+			
+		}, executionContext);
+	}
 }
