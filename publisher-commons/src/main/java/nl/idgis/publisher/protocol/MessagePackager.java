ffe43cf33392f4fe4cf4d26314dc77d63d001515
copierrj <reijer.copier@idgis.nl>
2015-01-27 18:03:20 +0100

    IsAlive removed
    caused receive timeout to be unusable

--- publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessagePackager.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessagePackager.java
@@ -3,13 +3,12 @@ package nl.idgis.publisher.protocol;
 import java.util.concurrent.TimeUnit;
 
 import nl.idgis.publisher.protocol.messages.Envelope;
-import nl.idgis.publisher.protocol.messages.IsAlive;
+
 import scala.concurrent.duration.Duration;
-import scala.concurrent.duration.FiniteDuration;
+
 import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Cancellable;
 import akka.actor.Props;
+import akka.actor.ReceiveTimeout;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -19,9 +18,8 @@ public class MessagePackager extends UntypedActor {
 	private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 	
 	private final String targetName, pathPrefix;
-	private final ActorRef messageTarget;
 	
-	private Cancellable deadWatch = null;
+	private final ActorRef messageTarget;
 
 	public MessagePackager(String targetName, ActorRef messageTarget, String pathPrefix) {
 		this.targetName = targetName;		
@@ -35,49 +33,32 @@ public class MessagePackager extends UntypedActor {
 	
 	@Override
 	public void preStart() {
-		deadWatch();
-	}
-	
-	private void deadWatch() {
-		if(deadWatch != null) {
-			deadWatch.cancel();
-		}
-		
-		ActorSystem system = getContext().system();
-		 
-		FiniteDuration duration = Duration.create(15, TimeUnit.SECONDS);
-		deadWatch = system.scheduler().schedule(duration, duration, messageTarget,
-			new IsAlive(targetName), system.dispatcher(), ActorRef.noSender());  
+		getContext().setReceiveTimeout(Duration.apply(2, TimeUnit.MINUTES));
 	}
 
 	@Override
 	public void onReceive(Object msg) throws Exception {
-		deadWatch();
+		if(msg instanceof ReceiveTimeout) {
+			getContext().stop(getSelf());
+		} else {		
+			final String sourceName;
 		
-		final String sourceName;
-		
-		ActorRef sender = getSender();
-		if(sender.equals(getContext().system().deadLetters())) {
-			log.debug("no sender");			
-			sourceName = null;
-		} else {			
-			log.debug("sender: " + sender);
-			String sourcePath = sender.path().toString();
-			if(sourcePath.startsWith(pathPrefix)) {
-				sourceName = sourcePath.substring(pathPrefix.length() + 1);
-			} else {
-				log.debug("sourcePath: " + sourcePath + " pathPrefix: " + pathPrefix + " msg: " + msg.toString());
-				throw new IllegalStateException("sender is not a child of container actor");
+			ActorRef sender = getSender();
+			if(sender.equals(getContext().system().deadLetters())) {
+				log.debug("no sender");			
+				sourceName = null;
+			} else {			
+				log.debug("sender: " + sender);
+				String sourcePath = sender.path().toString();
+				if(sourcePath.startsWith(pathPrefix)) {
+					sourceName = sourcePath.substring(pathPrefix.length() + 1);
+				} else {
+					log.debug("sourcePath: " + sourcePath + " pathPrefix: " + pathPrefix + " msg: " + msg.toString());
+					throw new IllegalStateException("sender is not a child of container actor");
+				}
 			}
-		}
-		
-		messageTarget.tell(new Envelope(targetName, msg, sourceName), getSelf());
-	}
-	
-	@Override
-	public void postStop() {
-		if(deadWatch != null) {
-			deadWatch.cancel();
+			
+			messageTarget.tell(new Envelope(targetName, msg, sourceName), getSelf());
 		}
 	}
 }
