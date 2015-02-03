f80b29b47432bfb44166a2d0f74083262d7d8277
Sandro Neumann <sandro.neumann@idgis.nl>
2015-02-03 13:33:20 +0100

    Added tiled layer form

--- publisher-web/app/controllers/Tiledlayers.java
+++ publisher-web/app/controllers/Tiledlayers.java
@@ -5,6 +5,7 @@ import play.libs.F.Promise;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import views.html.tiledlayers.form;
 import views.html.tiledlayers.list;
 import actions.DefaultAuthenticator;
 
@@ -12,6 +13,12 @@ import actions.DefaultAuthenticator;
 public class Tiledlayers extends Controller {
 
 	// CRUD
+	
+	public static Promise<Result> createForm () {
+		Logger.debug ("create Tiled layer");
+		
+		return Promise.pure (ok (form.render ()));
+	}
 	
 	public static Promise<Result> list () {
 		Logger.debug ("list Tiled lagen ");
