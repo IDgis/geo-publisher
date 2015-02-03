21be28934bb5b1fecc27bf6d7d0bef9f38cc6185
copierrj <reijer.copier@idgis.nl>
2015-02-03 12:57:29 +0100

    concurrency issues resolved

--- publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessageProtocolActors.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/protocol/MessageProtocolActors.java
@@ -1,39 +1,76 @@
 package nl.idgis.publisher.protocol;
 
+import java.io.Serializable;
+
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import nl.idgis.publisher.protocol.messages.Failure;
 import nl.idgis.publisher.protocol.messages.ListenerInit;
 import nl.idgis.publisher.protocol.messages.Register;
 import nl.idgis.publisher.protocol.messages.Registered;
-import akka.actor.ActorRef;
-import akka.actor.UntypedActor;
-import akka.dispatch.OnSuccess;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import akka.pattern.Patterns;
+import nl.idgis.publisher.utils.FutureUtils;
 
 public abstract class MessageProtocolActors extends UntypedActor {
+	
+	private static class CreateActors implements Serializable {
+	
+		private static final long serialVersionUID = -5194402567506379192L;
+		
+		private final ActorRef messagePackagerProvider;
+		
+		CreateActors(ActorRef messagePackagerProvider) {
+			this.messagePackagerProvider = messagePackagerProvider;
+		}
+		
+		ActorRef getMessagePackagerProvider() {
+			return messagePackagerProvider;
+		}
+
+		@Override
+		public String toString() {
+			return "CreateActors [messagePackagerProvider="
+					+ messagePackagerProvider + "]";
+		}
+	}
 	
 	private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 	
 	protected abstract void createActors(ActorRef messagePackagerProvider);
+	
+	protected void onReceiveElse(Object msg) {
+		unhandled(msg);
+	}
+	
+	protected FutureUtils f;
+	
+	@Override
+	public final void preStart() throws Exception {
+		f = new FutureUtils(getContext().dispatcher());
+	}
 
 	@Override
 	public final void onReceive(Object msg) throws Exception {
 		if(msg instanceof ListenerInit) {
-			ActorRef messageProtocolHandler = ((ListenerInit) msg).getMessagePackagerProvider();
-			Patterns.ask(messageProtocolHandler, new Register(getSelf()), 1000).onSuccess(new OnSuccess<Object>() {
-
-				@Override
-				public void onSuccess(Object o) throws Throwable {
-					Registered registered = (Registered)o;
+			ActorRef self = getSelf(), sender = getSender();	
+			
+			ActorRef messageProtocolHandler = ((ListenerInit)msg).getMessagePackagerProvider();
+			f.ask(messageProtocolHandler, new Register(getSelf()), Registered.class).whenComplete((registered, t) -> {
+				if(t != null) {
+					sender.tell(new Failure(t), self);
+				} else {				
 					ActorRef messagePackagerProvider = registered.getMessagePackagerProvider();
-					
-					log.debug("registered");
-					createActors(messagePackagerProvider);
-				}
 				
-			}, getContext().system().dispatcher());
+					log.debug("registered");
+					self.tell(new CreateActors(messagePackagerProvider), self);
+				}
+			});
+		} else if(msg instanceof CreateActors) {
+			createActors(((CreateActors)msg).getMessagePackagerProvider());
 		} else {
-			unhandled(msg);
+			onReceiveElse(msg);
 		}
 	}
 
--- publisher-provider/publisher-provider-app/src/main/java/nl/idgis/publisher/provider/ClientActors.java
+++ publisher-provider/publisher-provider-app/src/main/java/nl/idgis/publisher/provider/ClientActors.java
@@ -1,6 +1,9 @@
 package nl.idgis.publisher.provider;
 
 import java.io.File;
+import java.io.Serializable;
+
+import com.typesafe.config.Config;
 
 import nl.idgis.publisher.protocol.MessageProtocolActors;
 import nl.idgis.publisher.protocol.messages.GetMessagePackager;
@@ -8,18 +11,33 @@ import nl.idgis.publisher.protocol.messages.Hello;
 import nl.idgis.publisher.provider.database.Database;
 import nl.idgis.publisher.provider.metadata.Metadata;
 
-import scala.concurrent.Future;
-
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import akka.dispatch.OnSuccess;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
-import akka.pattern.Patterns;
-
-import com.typesafe.config.Config;
 
 public class ClientActors extends MessageProtocolActors {
+	
+	private static class CreateProviders implements Serializable {
+	
+		private static final long serialVersionUID = 3102092189552667733L;
+		
+		private final ActorRef harvester;
+		
+		CreateProviders(ActorRef harvester) {
+			this.harvester = harvester;
+		}
+
+		public ActorRef getHarvester() {
+			return harvester;
+		}
+
+		@Override
+		public String toString() {
+			return "CreateProviders [harvester=" + harvester + "]";
+		}
+		
+	}
 	
 	private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 	
@@ -33,26 +51,35 @@ public class ClientActors extends MessageProtocolActors {
 		return Props.create(ClientActors.class, config);
 	}
 	
+	@Override
+	public final void onReceiveElse(Object msg) {
+		if(msg instanceof CreateProviders) {
+			createProviders(((CreateProviders)msg).getHarvester());
+		} else {
+			unhandled(msg);
+		}	
+	}
+
+	private void createProviders(ActorRef harvester) {
+		log.debug("creating providers");
+		
+		for(Config instance : config.getConfigList("instances")) {
+			final String instanceName = instance.getString("name");					
+			
+			final Props database = Database.props(instance.getConfig("database"), instanceName);
+			final Props metadata = Metadata.props(new File(instance.getString("metadata.folder")));					
+			
+			final ActorRef provider = getContext().actorOf(Provider.props(database, metadata), instanceName);					
+			harvester.tell(new Hello(instanceName), provider);
+		}
+	} 
+	
 	protected void createActors(ActorRef messagePackagerProvider) {
 		log.debug("creating client actors");
-								
-		Future<Object> harvesterPackager = Patterns.ask(messagePackagerProvider, new GetMessagePackager("harvester"), 1000);
-		harvesterPackager.onSuccess(new OnSuccess<Object>() {
 
-			@Override
-			public void onSuccess(Object msg) {
-				final ActorRef harvester = (ActorRef)msg;
-				
-				for(Config instance : config.getConfigList("instances")) {
-					final String instanceName = instance.getString("name");					
-					
-					final Props database = Database.props(instance.getConfig("database"), instanceName);
-					final Props metadata = Metadata.props(new File(instance.getString("metadata.folder")));					
-					
-					final ActorRef provider = getContext().actorOf(Provider.props(database, metadata), instanceName);					
-					harvester.tell(new Hello(instanceName), provider);
-				}
-			}
-		}, getContext().system().dispatcher());
+		ActorRef self = getSelf();
+		f.ask(messagePackagerProvider, new GetMessagePackager("harvester"), ActorRef.class).thenAccept(harvester -> {
+			self.tell(new CreateProviders(harvester), self);
+		});
 	}
 }
