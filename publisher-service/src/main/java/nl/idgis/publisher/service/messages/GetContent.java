0dd85a2e80dc280db3e6fa405cf9137b91980bc7
copierrj <reijer.copier@idgis.nl>
2015-02-04 15:30:08 +0100

    service and metadata actors stripped
    to be reimplemented based on service information stored in database

--- publisher-service/src/main/java/nl/idgis/publisher/service/messages/GetContent.java
+++
@@ -1,8 +0,0 @@
-package nl.idgis.publisher.service.messages;
-
-import java.io.Serializable;
-
-public class GetContent implements Serializable {
-
-	private static final long serialVersionUID = -5051016116352586567L;
-}
