d80401bf081bb159b90b9ffb0415ac5dbfaa3c9b
copierrj <reijer.copier@idgis.nl>
2015-02-04 12:42:11 +0100

    transfer counter implemented

---
+++ publisher-commons/src/main/java/nl/idgis/publisher/protocol/messages/GetTransferedTotal.java
@@ -0,0 +1,14 @@
+package nl.idgis.publisher.protocol.messages;
+
+import java.io.Serializable;
+
+public class GetTransferedTotal implements Serializable {
+	
+	private static final long serialVersionUID = 2137186579577591744L;
+
+	@Override
+	public String toString() {
+		return "GetTransferedTotal []";
+	}
+
+}
