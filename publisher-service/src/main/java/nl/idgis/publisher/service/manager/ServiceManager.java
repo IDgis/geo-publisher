51e68827779859d862fb023fd729c3717aa88b48
copierrj <reijer.copier@idgis.nl>
2015-02-10 13:47:16 +0100

    service manager queries added

--- publisher-service/src/main/java/nl/idgis/publisher/service/manager/ServiceManager.java
+++ publisher-service/src/main/java/nl/idgis/publisher/service/manager/ServiceManager.java
@@ -1,6 +1,11 @@
 package nl.idgis.publisher.service.manager;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.mysema.query.sql.SQLSubQuery;
+import com.mysema.query.Tuple;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
@@ -9,14 +14,23 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
 import nl.idgis.publisher.database.AsyncDatabaseHelper;
+import nl.idgis.publisher.database.AsyncSQLQuery;
+import nl.idgis.publisher.database.QGenericLayer;
 
 import nl.idgis.publisher.service.manager.messages.DatasetNode;
 import nl.idgis.publisher.service.manager.messages.DefaultService;
 import nl.idgis.publisher.service.manager.messages.GetService;
 import nl.idgis.publisher.service.manager.messages.GroupNode;
 import nl.idgis.publisher.utils.FutureUtils;
+import static nl.idgis.publisher.database.QService.service;
+import static nl.idgis.publisher.database.QGenericLayer.genericLayer;
+import static nl.idgis.publisher.database.QLayerStructure.layerStructure;
+import static nl.idgis.publisher.database.QLeafLayer.leafLayer;
+import static nl.idgis.publisher.database.QDataSource.dataSource;
 
 public class ServiceManager extends UntypedActor {
+	
+	private final static QGenericLayer child = new QGenericLayer("child"), parent = new QGenericLayer("parent");
 	
 	private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 	
@@ -52,7 +66,38 @@ public class ServiceManager extends UntypedActor {
 	private void handleGetService(GetService msg) {
 		ActorRef sender = getSender(), self = getSelf();
 		
-		GroupNode root = new GroupNode("group0", "name0", "title0", "abstract0");
-		sender.tell(new DefaultService("service0", root, Collections.<DatasetNode>emptyList(), Collections.singletonList(root), Collections.<String, String>emptyMap()), self);
+		String serviceId = msg.getServiceId();
+		
+		db.transactional(tx -> {
+			AsyncSQLQuery structureQuery = tx.query().from(layerStructure)
+				.join(child).on(child.id.eq(layerStructure.childLayerId))
+				.join(parent).on(parent.id.eq(layerStructure.parentLayerId))
+				.where(new SQLSubQuery().from(service)
+					.where(service.identification.eq(serviceId)
+						.and(service.rootgroupId.eq(layerStructure.parentLayerId)))
+					.exists());
+				
+			return structureQuery.clone()
+				.orderBy(layerStructure.layerorder.asc())
+				.list(child.identification, parent.identification)
+				.thenApply(structureResult -> {
+					Map<String, String> structure = new LinkedHashMap<>();
+					
+					for(Tuple structureTuple : structureResult) {
+						structure.put(
+							structureTuple.get(child.identification),
+							structureTuple.get(parent.identification));
+					}
+					
+					tx.query().from(genericLayer)
+						.where(genericLayer.id.in(new SQLSubQuery().from(service)
+							.where(service.identification.eq(serviceId))
+							.list(service.rootgroupId)));
+					
+					GroupNode root = new GroupNode("group0", "name0", "title0", "abstract0");
+					return new DefaultService("service0", root, Collections.<DatasetNode>emptyList(), Collections.singletonList(root), structure);
+				});
+		}).thenAccept(resp -> sender.tell(resp, self));
+		
 	}
 }
--- publisher-service/src/test/java/nl/idgis/publisher/service/manager/ServiceManagerTest.java
+++ publisher-service/src/test/java/nl/idgis/publisher/service/manager/ServiceManagerTest.java
@@ -1,15 +1,134 @@
 package nl.idgis.publisher.service.manager;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.Test;
 
+import com.mysema.query.sql.SQLSubQuery;
+
 import nl.idgis.publisher.service.manager.messages.GetService;
+import nl.idgis.publisher.service.manager.messages.Layer;
 import nl.idgis.publisher.service.manager.messages.Service;
 import nl.idgis.publisher.AbstractServiceTest;
+import static nl.idgis.publisher.database.QDataSource.dataSource;
+import static nl.idgis.publisher.database.QCategory.category;
+import static nl.idgis.publisher.database.QSourceDataset.sourceDataset;
+import static nl.idgis.publisher.database.QSourceDatasetVersion.sourceDatasetVersion;
+import static nl.idgis.publisher.database.QSourceDatasetVersionColumn.sourceDatasetVersionColumn;
+import static nl.idgis.publisher.database.QDataset.dataset;
+import static nl.idgis.publisher.database.QDatasetColumn.datasetColumn;
+import static nl.idgis.publisher.database.QJob.job;
+import static nl.idgis.publisher.database.QImportJob.importJob;
+import static nl.idgis.publisher.database.QImportJobColumn.importJobColumn;
+import static nl.idgis.publisher.database.QService.service;
+import static nl.idgis.publisher.database.QGenericLayer.genericLayer;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class ServiceManagerTest extends AbstractServiceTest {
 
 	@Test
 	public void testGetService() throws Exception {
-		sync.ask(serviceManager, new GetService("service0"), Service.class);
+		int dataSourceId = insert(dataSource)
+			.set(dataSource.identification, "datasource0")
+			.set(dataSource.name, "datasource-name0")
+			.executeWithKey(dataSource.id);
+		
+		int categoryId = insert(category)
+			.set(category.identification, "category0")
+			.set(category.name, "category-name0")
+			.executeWithKey(category.id);
+		
+		int sourceDatasetId = insert(sourceDataset)
+			.set(sourceDataset.identification, "sourcedataset0")
+			.set(sourceDataset.dataSourceId, dataSourceId)
+			.executeWithKey(sourceDataset.id);
+		
+		int sourceDatasetVersionId = insert(sourceDatasetVersion)
+			.set(sourceDatasetVersion.sourceDatasetId, sourceDatasetId)
+			.set(sourceDatasetVersion.categoryId, categoryId)
+			.set(sourceDatasetVersion.name, "sourcedataset-name0")
+			.set(sourceDatasetVersion.type, "VECTOR")
+			.executeWithKey(sourceDatasetVersion.id);
+		
+		for(int i = 0; i < 10; i++) {
+			insert(sourceDatasetVersionColumn)
+			.set(sourceDatasetVersionColumn.sourceDatasetVersionId, sourceDatasetVersionId)
+			.set(sourceDatasetVersionColumn.index, i)
+			.set(sourceDatasetVersionColumn.name, "column" + i)
+			.set(sourceDatasetVersionColumn.dataType, "TEXT")
+			.execute();
+		}
+		
+		int datasetId = insert(dataset)
+			.set(dataset.identification, "dataset0")
+			.set(dataset.name, "dataset-name0")
+			.set(dataset.fileUuid, UUID.randomUUID().toString())
+			.set(dataset.uuid, UUID.randomUUID().toString())
+			.set(dataset.sourceDatasetId, sourceDatasetId)
+			.executeWithKey(dataset.id);
+		
+		insert(datasetColumn)
+			.columns(
+				datasetColumn.datasetId,
+				datasetColumn.index,
+				datasetColumn.name,
+				datasetColumn.dataType)
+			.select(new SQLSubQuery().from(sourceDatasetVersionColumn)
+				.where(sourceDatasetVersionColumn.sourceDatasetVersionId.eq(sourceDatasetVersionId))
+				.list(
+					datasetId,
+					sourceDatasetVersionColumn.index,
+					sourceDatasetVersionColumn.name,
+					sourceDatasetVersionColumn.dataType))
+			.execute();
+		
+		int jobId = insert(job)
+			.set(job.type, "IMPORT")
+			.executeWithKey(job.id);
+		
+		int importJobId = insert(importJob)
+			.set(importJob.jobId, jobId)
+			.set(importJob.sourceDatasetVersionId, sourceDatasetVersionId)
+			.set(importJob.datasetId, datasetId)
+			.set(importJob.filterConditions, "{ \"expression\": null }")
+			.executeWithKey(importJob.id);
+		
+		insert(importJobColumn)
+			.columns(
+				importJobColumn.importJobId,
+				importJobColumn.index,
+				importJobColumn.name,
+				importJobColumn.dataType)
+			.select(new SQLSubQuery().from(datasetColumn)
+				.where(datasetColumn.datasetId.eq(datasetId))
+				.list(
+					importJobId,
+					datasetColumn.index,
+					datasetColumn.name,
+					datasetColumn.dataType))
+			.execute();
+		
+		int rootGroupId = insert(genericLayer)
+			.set(genericLayer.identification, "rootgroup")
+			.set(genericLayer.name, "rootgroup-name")
+			.executeWithKey(genericLayer.id);
+		
+		insert(service)
+			.set(service.identification, "service0")
+			.set(service.rootgroupId, rootGroupId) 
+			.execute();
+		
+		Service service = sync.ask(serviceManager, new GetService("service0"), Service.class);		
+		assertEquals("rootgroup", service.getRootId());
+		
+		List<Layer> layers = service.getLayers();
+		assertNotNull(layers);
+		
+		Iterator<Layer> itr = layers.iterator();
+		assertFalse(itr.hasNext());
 	}
 }
