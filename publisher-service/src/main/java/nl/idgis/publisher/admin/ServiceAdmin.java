38c92dae129503e89daab71b75b246c9e95ee29c
RobStek <rob.stekelenburg@idgis.nl>
2015-02-10 09:31:27 +0100

    Added service admin crud methods

--- publisher-service/src/main/java/nl/idgis/publisher/admin/ServiceAdmin.java
+++ publisher-service/src/main/java/nl/idgis/publisher/admin/ServiceAdmin.java
@@ -1,5 +1,15 @@
 package nl.idgis.publisher.admin;
 
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static nl.idgis.publisher.database.QService.service;
+import nl.idgis.publisher.domain.response.Page;
+import nl.idgis.publisher.domain.response.Response;
+import nl.idgis.publisher.domain.service.CrudOperation;
+import nl.idgis.publisher.domain.service.CrudResponse;
+import nl.idgis.publisher.domain.web.Service;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 
@@ -15,7 +25,96 @@ public class ServiceAdmin extends AbstractAdmin {
 
 	@Override
 	protected void preStartAdmin() {
+		addList(Service.class, this::handleListServices);
+		addGet(Service.class, this::handleGetService);
+		addPut(Service.class, this::handlePutService);
+		addDelete(Service.class, this::handleDeleteService);
 	}
 
+	private CompletableFuture<Page<Service>> handleListServices () {
+		log.debug ("handleListServices");
+		
+		return 
+			db.query().from(service)
+			.list(new nl.idgis.publisher.domain.web.QService(
+					service.identification,
+					service.name,
+					service.title, 
+					service.alternatetitle, 
+					service.abstractCol,
+					service.metadata,
+					service.keywords,
+					service.watermark))
+			.thenApply(this::toPage);
+	}
+
+	
+	private CompletableFuture<Optional<Service>> handleGetService (String serviceId) {
+		log.debug ("handleGetService: " + serviceId);
+		
+		return 
+			db.query().from(service)
+			.where(service.identification.eq(serviceId))
+			.singleResult(new nl.idgis.publisher.domain.web.QService(
+					service.identification,
+					service.name,
+					service.title, 
+					service.alternatetitle, 
+					service.abstractCol,
+					service.metadata,
+					service.keywords,
+					service.watermark
+			));		
+	}
+	
+	private CompletableFuture<Response<?>> handlePutService(Service theService) {
+		String serviceId = theService.id();
+		String serviceName = theService.name();
+		log.debug ("handle update/create service: " + serviceId);
+		
+		return db.transactional(tx ->
+			// Check if there is another service with the same name
+			tx.query().from(service)
+			.where(service.name.eq(serviceId))
+			.singleResult(service.name)
+			.thenCompose(msg -> {
+				if (!msg.isPresent()){
+					// INSERT
+					log.debug("Inserting new service with name: " + serviceName);
+					return tx.insert(service)
+					.set(service.identification, UUID.randomUUID().toString())
+					.set(service.name, serviceName)
+					.set(service.title, theService.title())
+					.set(service.alternatetitle, theService.alternateTitle())
+					.set(service.abstractCol, theService.abstractText())
+					.set(service.metadata, theService.metadata())
+					.set(service.keywords, theService.keywords())
+					.set(service.watermark, theService.watermark())
+					.execute()
+					.thenApply(l -> new Response<String>(CrudOperation.CREATE, CrudResponse.OK, serviceName));
+				} else {
+					// UPDATE
+					log.debug("Updating service with name: " + serviceName);
+					return tx.update(service)
+							.set(service.title, theService.title())
+							.set(service.alternatetitle, theService.alternateTitle())
+							.set(service.abstractCol, theService.abstractText())
+							.set(service.metadata, theService.metadata())
+							.set(service.keywords, theService.keywords())
+							.set(service.watermark, theService.watermark())
+					.where(service.identification.eq(serviceId))
+					.execute()
+					.thenApply(l -> new Response<String>(CrudOperation.UPDATE, CrudResponse.OK, serviceName));
+				}
+		}));
+	}
+
+	private CompletableFuture<Response<?>> handleDeleteService(String serviceId) {
+		log.debug ("handleDeleteService: " + serviceId);
+		return db.delete(service)
+			.where(service.identification.eq(serviceId))
+			.execute()
+			.thenApply(l -> new Response<String>(CrudOperation.DELETE, CrudResponse.OK, serviceId));
+	}
 	
 }
