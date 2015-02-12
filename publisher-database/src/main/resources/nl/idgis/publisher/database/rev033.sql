1e2c85b61d09cd621ddfb55758d76db75bda4394
RobStek <rob.stekelenburg@idgis.nl>
2015-02-12 11:57:39 +0100

    database: script adaptation for H2

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev033.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev033.sql
@@ -3,11 +3,6 @@
 -- 
 
 -- column rename
-alter table publisher.service drop column defaultcategory_id;
-alter table publisher.service add column default_category_id  references publisher.category(id);
-
-alter table publisher.service drop column application_id;
-alter table publisher.service add column constants_id integer references publisher.constants(id);
 
 alter table publisher.layer_style drop column defaultstyle;
 alter table publisher.layer_style add column default_style boolean default false;
@@ -15,14 +10,33 @@ alter table publisher.layer_style add column default_style boolean default false
 alter table publisher.layer_structure drop column layerorder;
 alter table publisher.layer_structure add column layer_order integer default 0;
 
+-- table service 
+drop table publisher.service;
+create table publisher.service(
+	id serial primary key,
+	identification text,
+	name text,
+	title text,
+	alternate_title text,
+	abstract text,
+	keywords text,
+	metadata text,
+	watermark text,
+	rootgroup_id integer references publisher.generic_layer(id),
+	default_category_id integer references publisher.category(id),
+	constants_id integer references publisher.constants(id),
+	-- add watermark
+	watermark_enabled boolean default false,
+	watermark_url text,
+	watermark_transparency integer,
+	watermark_position text
+);
+
+
+
 -- column remove
 alter table publisher.leaf_layer drop column identification;
 
--- add watermark
-alter table publisher.service add column watermark_enabled boolean default false;
-alter table publisher.service add column watermark_url text;
-alter table publisher.service add column watermark_transparency integer;
-alter table publisher.service add column watermark_position text;
 
 insert into publisher.version(id) values(33);
 -- ----------------------------------
