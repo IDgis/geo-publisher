a52f675b28a22a24e6a151fef6d73029a71791c7
RobStek <rob.stekelenburg@idgis.nl>
2015-02-12 11:25:01 +0100

    database: column renames, remove identification, add watermark

---
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev033.sql
@@ -0,0 +1,26 @@
+-- 
+-- several column renames, column remove
+-- 
+
+-- column rename
+alter table publisher.service drop column defaultcategory_id;
+alter table publisher.service add column default_category_id  references publisher.category(id);
+
+alter table publisher.service drop column application_id;
+alter table publisher.service add column constants_id integer references publisher.constants(id);
+
+alter table publisher.layer_style drop column defaultstyle;
+alter table publisher.layer_style add column default_style boolean default false;
+
+alter table publisher.layer_structure drop column layerorder;
+alter table publisher.layer_structure add column layer_order integer default 0;
+
+-- column remove
+alter table publisher.leaf_layer drop column identification;
+
+-- add watermark
+alter table publisher.service add column watermark_enabled boolean default false;
+alter table publisher.service add column watermark_url text;
+alter table publisher.service add column watermark_transparency integer;
+alter table publisher.service add column watermark_position text;
+
