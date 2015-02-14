7f61c51fdbf2cb068e6cca477e4cbd24285e150d
copierrj <reijer.copier@idgis.nl>
2015-02-14 16:00:05 +0100

    start service provisioning on layer update

--- publisher-service/src/main/java/nl/idgis/publisher/admin/JobCreator.java
+++ publisher-service/src/main/java/nl/idgis/publisher/admin/JobCreator.java
@@ -3,9 +3,13 @@ package nl.idgis.publisher.admin;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 
+import nl.idgis.publisher.domain.web.Layer;
+import nl.idgis.publisher.domain.web.LayerGroup;
 import nl.idgis.publisher.domain.web.Service;
 
 import nl.idgis.publisher.job.manager.messages.CreateServiceJob;
+import nl.idgis.publisher.service.manager.messages.GetServicesWithLayer;
+import nl.idgis.publisher.utils.TypedIterable;
 
 public class JobCreator extends AbstractAdmin {
 	
@@ -27,10 +31,27 @@ public class JobCreator extends AbstractAdmin {
 		
 		jobSystem.tell(new CreateServiceJob(serviceId), getSelf());
 	}
+	
+	private void createServiceJobsForLayer(String layerId) {
+		log.debug("creating service jobs for layer: {}", layerId);
+		
+		f.ask(serviceManager, new GetServicesWithLayer(layerId), TypedIterable.class).thenAccept(resp -> {			
+			TypedIterable<String> serviceIds = ((TypedIterable<?>)resp).cast(String.class);
+			
+			for(String serviceId : serviceIds) {
+				log.debug("creating service job: {}", serviceId);
+				
+				jobSystem.tell(new CreateServiceJob(serviceId), getSelf());
+			}
+		});
+	}
 
 	@Override
 	protected void preStartAdmin() {
 		onPut(Service.class, service -> createServiceJob(service.id()));
+		
+		onPut(Layer.class, layer -> createServiceJobsForLayer(layer.id()));
+		onPut(LayerGroup.class, layer -> createServiceJobsForLayer(layer.id()));
 	}
 
 }
