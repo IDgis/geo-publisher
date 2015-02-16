c0187b97cfa0c18c534cd0491c7692a98b3bbe6e
copierrj <reijer.copier@idgis.nl>
2015-02-16 16:53:43 +0100

    equals and hashCode implemented
    used to compare layer groups

--- publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/LayerRef.java
+++ publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/LayerRef.java
@@ -20,6 +20,34 @@ public class LayerRef {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (group ? 1231 : 1237);
+		result = prime * result + ((layerId == null) ? 0 : layerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LayerRef other = (LayerRef) obj;
+		if (group != other.group)
+			return false;
+		if (layerId == null) {
+			if (other.layerId != null)
+				return false;
+		} else if (!layerId.equals(other.layerId))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "LayerRef [layerId=" + layerId + ", group=" + group + "]";
 	}
