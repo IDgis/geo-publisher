7a27ba6a2c0ad42fad6d52e19cc348e77c573629
RobStek <rob.stekelenburg@idgis.nl>
2015-02-20 12:39:31 +0100

    several fixes

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev036.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev036.sql
@@ -1,56 +1,57 @@
 --
--- Sevaral database changes  
---
-
---
--- Additional tables
---
-
--- table service keywords
-create table publisher.service_keyword(
-	keyword text,
-	service_id integer references publisher.service(id),
-);
-
--- table layer keywords
-create table publisher.leaf_layer_keyword(
-	keyword text,
-	leaf_layer_id integer references publisher.leaf_layer(id),
-);
-
--- table tiledlayer mimeformats
-create table publisher.tiled_layer_mimeformat(
-	mimeformat text,
-	tiled_layer_id integer references publisher.tiled_layer(id),
-);
-
+-- Several database changes  
 --
 -- changing columns (removing / renaming / adding)
 --
-alter table publisher.service drop column watermark;
-alter table publisher.service drop column default_category_id;
--- rename rootgroup_id to generic_layer_id
-alter table publisher.service drop column rootgroup_id;
-alter table publisher.service add column generic_layer_id integer references generic_layer(id);
-
+-- tables with new references need drop/create because of H2 
+drop table publisher.service;
+create table publisher.service(
+	id serial primary key,
+	identification text,
+	name text,
+	title text,
+	alternate_title text,
+	abstract text,
+	metadata text,
+	generic_layer_id integer references publisher.generic_layer(id),
+	constants_id integer references publisher.constants(id),
+	-- add watermark
+	watermark_enabled boolean default false,
+	watermark_url text,
+	watermark_transparency integer,
+	watermark_position text,
+	published boolean default false
+);
+ 
 alter table publisher.tiled_layer drop column identification;
 alter table publisher.tiled_layer drop column mime_formats;
 
 alter table publisher.constants drop column name;
 alter table publisher.constants drop column url;
 
-alter table publisher.generic_layer add column style_id integer references style(id);
+alter table publisher.style drop column format;
+alter table publisher.style drop column version;
+
+alter table publisher.leaf_layer drop column keywords;
 
 --
--- add foreign key references 
+-- Additional tables
 --
-
-
-alter table publisher.service add constraint service_identification_key unique(identification);
-
-alter table publisher.layer_structure add constraint layer_structure_style_id_fk foreign key (style_id) references publisher.style(id);
-
-
+-- table service keywords
+create table publisher.service_keyword(
+	keyword text,
+	service_id integer references publisher.service(id)
+);
+-- table layer keywords
+create table publisher.leaf_layer_keyword(
+	keyword text,
+	leaf_layer_id integer references publisher.leaf_layer(id)
+);
+-- table tiledlayer mimeformats
+create table publisher.tiled_layer_mimeformat(
+	mimeformat text,
+	tiled_layer_id integer references publisher.tiled_layer(id)
+);
 
 insert into publisher.version(id) values(36);
 -- -----------------------------------------
