cf3fa12d850972d88cacca5bdecf8f9a4b15ff57
copierrj <reijer.copier@idgis.nl>
2015-02-27 16:35:39 +0100

    hashCode, equals and toString added

--- publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/GroupRef.java
+++ publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/GroupRef.java
@@ -16,4 +16,9 @@ public class GroupRef extends PublishedRef {
 		return this;
 	}
 
+	@Override
+	public String toString() {
+		return "GroupRef [layerName=" + layerName + "]";
+	}
+
 }
--- publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/PublishedRef.java
+++ publisher-service/src/main/java/nl/idgis/publisher/service/geoserver/rest/PublishedRef.java
@@ -2,7 +2,7 @@ package nl.idgis.publisher.service.geoserver.rest;
 
 public abstract class PublishedRef {
 	
-	private final String layerName;
+	protected final String layerName;
 	
 	protected PublishedRef(String layerName) {
 		this.layerName = layerName;
@@ -20,5 +20,31 @@ public abstract class PublishedRef {
 	
 	public String getLayerName() {
 		return layerName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((layerName == null) ? 0 : layerName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PublishedRef other = (PublishedRef) obj;
+		if (layerName == null) {
+			if (other.layerName != null)
+				return false;
+		} else if (!layerName.equals(other.layerName))
+			return false;
+		return true;
 	}
 }
