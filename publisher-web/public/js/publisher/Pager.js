353b5ffc24db7718113527747618a0b30c81a9b0
Erik Orbons <erikorbons@users.noreply.github.com>
2015-03-02 16:28:19 +0100

    Implemented crude filtering in pager control for styles.

--- publisher-web/public/js/publisher/Pager.js
+++ publisher-web/public/js/publisher/Pager.js
@@ -1,5 +1,7 @@
 define ([
 	'dojo/_base/lang',
+	'dojo/_base/array',
+	
 	'dojo/query',
 	'dojo/on',
 	'dojo/dom-attr',
@@ -9,6 +11,8 @@ define ([
 	'put-selector/put'
 ], function (
 	lang,
+	array,
+	
 	query,
 	on,
 	domAttr,
@@ -20,6 +24,8 @@ define ([
 	
 	function Pager (nodeOrSelector) {
 		this.node = query (nodeOrSelector)[0];
+		this._handlers = [ ];
+		this._currentValues = { };
 		
 		var self = this;
 		
@@ -32,16 +38,81 @@ define ([
 				self._doPage (url);
 			}
 		});
+		
+		this._bind ();
 	}
 	
 	lang.mixin (Pager.prototype, {
 		_doPage: function (url) {
-			console.log ('Paging to: ', url);
-			xhr.get (url, {
+			this._update (url);
+		},
+		
+		_scheduleUpdate: function (url) {
+			if (this._scheduleTimeout) {
+				clearTimeout (this._scheduleTimeout);
+			}
+			
+			this._scheduleTimeout = setTimeout (lang.hitch (this, function () {
+				this._update (url);
+			}), 100);
+		},
+		
+		_update: function (url) {
+			var parameters = '';
+			
+			// Query pager parameters:
+			query ('select[data-pager-parameter],input[data-pager-parameter]', this.node).forEach (function (node) {
+				var name = domAttr.get (node, 'data-pager-parameter'),
+					value = node.value;
+
+				if (lang.trim (value) != '') {
+					if (parameters.length == 0 && !url.contains ('?')) {
+						parameters += '?';
+					} else {
+						parameters += '&';
+					}
+					
+					parameters += name + '=';
+					parameters += encodeURIComponent (value);
+				}
+			});
+			
+			xhr.get (url + parameters, {
 				handleAs: 'json'
 			}).then (lang.hitch (this, function (data) {
 				domConstruct.empty (this.node);
 				this.node.innerHTML = data.htmlContent;
+				this._bind ();
+			}));
+		},
+		
+		_bind: function () {
+			array.forEach (this._handlers, function (handler) {
+				handler.remove ();
+			});
+			
+			this._handlers = [ ];
+			this._currentValues = { };
+			
+			query ('select[data-pager-parameter],input[data-pager-parameter]', this.node).forEach (lang.hitch (this, function (node) {
+				var name = domAttr.get (node, 'data-pager-parameter');
+				
+				this._handlers.push (on (node, 'change,keydown,keyup', lang.hitch (this, function (e) {
+					var currentValue = lang.trim (node.value);
+					
+					if (!(name in this._currentValues) || this._currentValues[name] !== currentValue) {
+						this._changeValue (name, lang.trim (node.value));
+						this._currentValues[name] = currentValue;
+					}
+				})));
+				
+				this._currentValues[name] = lang.trim (node.value || '');
+			}));
+		},
+		
+		_changeValue: function (name, value) {
+			query ('*[data-pager-url]', this.node).forEach (lang.hitch (this, function (node) {
+				this._scheduleUpdate (domAttr.get (node, 'data-pager-url'));
 			}));
 		}
 	});
