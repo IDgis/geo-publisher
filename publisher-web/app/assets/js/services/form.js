c50172eff2f2b9cf960518bbdcc62cc7934cadf8
RobStek <rob.stekelenburg@idgis.nl>
2015-03-02 09:54:52 +0100

    Add interaction of keyword choice to layer, style forms

--- publisher-web/app/assets/js/services/form.js
+++ publisher-web/app/assets/js/services/form.js
@@ -1,3 +1,45 @@
+require ([
+	'dojo/dom',
+	'dojo/on',
+	'dojo/_base/window',
+	'dojo/query',
+	'dojo/dom-attr',
+	'dojo/dom-construct',
+	'put-selector/put',
+	
+	'dojo/NodeList-traverse',
+	
+	'dojo/domReady!'
+], function (dom, on, win, query, domattr, domConstruct, put) {
+	var keywordbutton = dom.byId('add-keyword');
+	var keywordlist = dom.byId('keyword-list');
+	
+	on(keywordbutton,'click', function(evt) {
+		var keywordinput = dom.byId('input-keywords').value;
+		
+		if(keywordinput !== "") {
+			var el1 = put("div.keyword-item-block[value=$]", keywordinput);
+			var el2a = put(el1, "input[type=hidden][name=$][value=$]", "keywords[]", keywordinput);
+			var el2b = put(el1, "div.keyword-item.label.label-primary", keywordinput);
+			var el3 = put(el2b, "button.close[type=button][aria-hidden=true][value=$]", keywordinput);
+			el3.innerHTML = "&times;";
+			
+			put(keywordlist, el1);
+		}
+		
+		dom.byId('input-keywords').value = "";
+	});
+	
+	on(win.doc, ".close:click", function(event) {
+		
+		
+		var valueItem = domattr.get(this, 'value');
+		
+		var itemToDel = query(this).parents(".keyword-item-block")[0];
+		domConstruct.destroy(itemToDel);
+	});
+});
+
 $(function () {
 	$('[data-toggle="popover"]').popover();
 });
