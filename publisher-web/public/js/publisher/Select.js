10a1427f935532c8e9caf00971b04c88010edd26
Erik Orbons <erikorbons@users.noreply.github.com>
2015-03-03 12:22:59 +0100

    Select dropdown can be closed by clicking outside.

--- publisher-web/public/js/publisher/Select.js
+++ publisher-web/public/js/publisher/Select.js
@@ -49,10 +49,33 @@ define ([
 	lang.mixin (Select.prototype, {
 		open: function () {
 			query ('.dropdown', this.node).addClass ('open');
+			
+			var self = this;
+			
+			this._closeHandle = on (query ('html')[0], 'click', function (e) {
+				var node = e.target,
+					dropdown = query ('.dropdown', self.node)[0],
+					button = query ('.js-pager-select-button')[0];
+				
+				while (node) {
+					if (node == dropdown || node == button) {
+						return;
+					}
+					
+					node = node.parentNode;
+				}
+				
+				self.close ();
+			});
 		},
 		
 		close: function () {
 			query ('.dropdown', this.node).removeClass ('open');
+			
+			if (this._closeHandle) {
+				this._closeHandle.remove ();
+				delete this._closeHandle;
+			}
 		},
 		
 		onSelect: function (item) {
