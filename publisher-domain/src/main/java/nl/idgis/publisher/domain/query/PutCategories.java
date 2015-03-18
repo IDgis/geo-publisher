541388365f0275b39503ed3d3a7f74705c7af944
RobStek <rob.stekelenburg@idgis.nl>
2015-03-18 12:52:21 +0100

    organize imports

--- publisher-domain/src/main/java/nl/idgis/publisher/domain/query/PutCategories.java
+++ publisher-domain/src/main/java/nl/idgis/publisher/domain/query/PutCategories.java
@@ -4,12 +4,6 @@ import java.util.List;
 
 import nl.idgis.publisher.domain.response.Response;
 import nl.idgis.publisher.domain.web.Category;
-import nl.idgis.publisher.domain.web.Identifiable;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mysema.query.annotations.QueryProjection;
 
 public final class PutCategories implements DomainQuery<Response<?>> {
 	
