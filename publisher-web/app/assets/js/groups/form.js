86b56e3f43eb2735bdd629572b277b1d4d8861af
copierrj <reijer.copier@idgis.nl>
2015-03-19 10:09:11 +0100

    dutch variable names removed

--- publisher-web/app/assets/js/groups/form.js
+++ publisher-web/app/assets/js/groups/form.js
@@ -37,9 +37,6 @@ require ([
 		var list = new OrderedList ('#groupLayerStructure');
 	}
 	
-	var groepen = dom.byId('addgroup');
-	var lagen = dom.byId('addlayer');
-	
 	var inputEnable = dom.byId('input-enable');
 	var jsTiledForm = dom.byId('js-tiled-form');
 	if (inputEnable.checked) {
@@ -48,21 +45,21 @@ require ([
 		domStyle.set(jsTiledForm, "display", "none");
 	}
 	
-	on(groepen,'change', function(evt) {
+	on(dom.byId('addgroup'),'change', function(evt) {
 		var selectIndex = evt.currentTarget.options.selectedIndex;
 		
 		var id = evt.currentTarget[selectIndex].value;
-		var naam = evt.currentTarget[selectIndex].innerHTML;
+		var name = evt.currentTarget[selectIndex].innerHTML;
 		var structureName="structure[]";
 		
-		if(naam !== "") {
+		if(name !== "") {
 			var el1 = put("div.list-group-item.gp-draggable.tree-item[value=$]", id);
 			var el2 = put(el1, "input[type=hidden][name=$][value=$]", structureName, id);
 			var el3 = put(el1, "div.row");
 			var el4 = put(el3, "div.col-sm-11.groupTree");
 			var el5 = put(el4, "ul.treelist");
 			var el6 = put(el5, "li");
-			var el7 = put(el6, "a", naam);
+			var el7 = put(el6, "a", name);
 			var el8 = put(el3, "div.col-sm-1");
 			var el9 = put(el8, "div.pull-right.tree-item-delete");
 			var el10 = put(el9, "a.btn.btn-warning.btn-sm.delete-el[value=$]", id);
@@ -73,17 +70,17 @@ require ([
 	});
 	
 	function addLayer (id, label) {
-		var naam = label;
+		var name = label;
 		var structureName="structure[]";
 		
-		if(naam !== "") {
+		if(name !== "") {
 			var el1 = put("div.list-group-item.gp-draggable.tree-item[value=$]", id);
 			var el2 = put(el1, "input[type=hidden][name=$][value=$]", structureName, id);
 			var el3 = put(el1, "div.row");
 			var el4 = put(el3, "div.col-sm-11.groupTree");
 			var el5 = put(el4, "ul.treelist");
 			var el6 = put(el5, "li");
-			var el7 = put(el6, "a", naam);
+			var el7 = put(el6, "a", name);
 			var el8 = put(el3, "div.col-sm-1");
 			var el9 = put(el8, "div.pull-right.tree-item-delete");
 			var el10 = put(el9, "a.btn.btn-warning.btn-sm.delete-el[value=$]", id);
