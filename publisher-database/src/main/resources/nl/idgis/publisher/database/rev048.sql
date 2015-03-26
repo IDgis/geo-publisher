d14afd70e3b0e7dab817533b50e0ff3b5837f187
copierrj <reijer.copier@idgis.nl>
2015-03-26 13:09:13 +0100

    published_service tables fixes

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev048.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev048.sql
@@ -1,6 +1,6 @@
 
 create table publisher.environment(
-	id integer,
+	id serial,
 	identification varchar(80),
 	name varchar(200),
 
@@ -8,21 +8,20 @@ create table publisher.environment(
 );
 
 create table publisher.published_service(
-	id integer,
 	service_id integer,
 	create_time timestamp default now(),
 	content varchar(131072), -- 128 KiB	
 
-	constraint published_service_pk primary key(id),
-	constraint published_service_service_fk foreign key(service_id) references publisher.environment(id)
+	constraint published_service_pk primary key(service_id),
+	constraint published_service_service_fk foreign key(service_id) references publisher.service(id)
 );
 
 create table publisher.published_service_environment(
-	published_service_id integer,
+	service_id integer,
 	environment_id integer,
 	
-	constraint published_service_environment_pk primary key(published_service_id, environment_id),
-	constraint published_service_environment_published_service_id_fk foreign key(published_service_id) references publisher.published_service(id),
+	constraint published_service_environment_pk primary key(service_id, environment_id),
+	constraint published_service_environment_service_id_fk foreign key(service_id) references publisher.published_service(service_id),
 	constraint published_service_environment_environment_id foreign key(environment_id) references publisher.environment(id)
 );
 
