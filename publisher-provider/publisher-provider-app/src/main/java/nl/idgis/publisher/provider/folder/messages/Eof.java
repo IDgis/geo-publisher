2b3a8725bfb5446db4b03ab687bced4bde82a688
copierrj <reijer.copier@idgis.nl>
2015-04-03 15:13:54 +0200

    mssing serialVersionUID added

--- publisher-provider/publisher-provider-app/src/main/java/nl/idgis/publisher/provider/folder/messages/Eof.java
+++ publisher-provider/publisher-provider-app/src/main/java/nl/idgis/publisher/provider/folder/messages/Eof.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class Eof implements Serializable {
 
+	private static final long serialVersionUID = -6860261420687054597L;
+
 	@Override
 	public String toString() {
 		return "Eof []";
