7d8d4b094a5b608c61bcd10b1080550363cc837c
copierrj <reijer.copier@idgis.nl>
2015-04-03 14:09:28 +0200

    ProviderFetchRasterDatasetInitiator implemented

---
+++ publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderFetchDatasetInitiator.java
@@ -0,0 +1,68 @@
+package nl.idgis.publisher.harvester.sources;
+
+import java.util.concurrent.TimeUnit;
+
+import akka.actor.ActorRef;
+import akka.actor.ReceiveTimeout;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import scala.concurrent.duration.Duration;
+
+import nl.idgis.publisher.harvester.sources.messages.FetchDataset;
+import nl.idgis.publisher.harvester.sources.messages.StartImport;
+import nl.idgis.publisher.protocol.messages.Ack;
+import nl.idgis.publisher.provider.protocol.AbstractGetDatasetRequest;
+import nl.idgis.publisher.provider.protocol.DatasetInfo;
+import nl.idgis.publisher.provider.protocol.VectorDatasetInfo;
+
+public abstract class ProviderFetchDatasetInitiator<T extends FetchDataset, U extends DatasetInfo> extends UntypedActor {
+
+	protected final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+	
+	private final Duration timeout = Duration.create(15, TimeUnit.SECONDS);
+	
+	protected final ActorRef sender, receiver, provider;
+	
+	protected final T request;
+	
+	public ProviderFetchDatasetInitiator(ActorRef sender, T request, ActorRef receiver, ActorRef provider) {
+		this.sender = sender;
+		this.request = request;
+		this.receiver = receiver;
+		this.provider = provider;
+	}
+	
+	@Override
+	public void preStart() throws Exception {
+		getContext().setReceiveTimeout(timeout);
+	}
+	
+	protected abstract StartImport startImport(U datasetInfo);
+	
+	protected abstract AbstractGetDatasetRequest getDataset();
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public void onReceive(Object msg) throws Exception {
+		if(msg instanceof ReceiveTimeout) {
+			log.error("timeout");
+			
+			getContext().stop(getSelf());
+		} else if(msg instanceof VectorDatasetInfo) {
+			log.debug("dataset info received");
+			
+			receiver.tell(startImport((U)msg), getSelf());
+		} else if(msg instanceof Ack) {
+			log.debug("receiver is ready");
+			
+			provider.tell(getDataset(), receiver);
+			
+			getContext().stop(getSelf());
+		} else {
+			unhandled(msg);
+		}
+		
+	}
+}
--- publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderFetchVectorDatasetInitiator.java
+++ publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderFetchVectorDatasetInitiator.java
@@ -1,40 +1,21 @@
 package nl.idgis.publisher.harvester.sources;
 
-import java.util.concurrent.TimeUnit;
-
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import akka.actor.ReceiveTimeout;
-import akka.actor.UntypedActor;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-
-import scala.concurrent.duration.Duration;
 
 import nl.idgis.publisher.harvester.sources.messages.FetchVectorDataset;
+import nl.idgis.publisher.harvester.sources.messages.StartImport;
 import nl.idgis.publisher.harvester.sources.messages.StartVectorImport;
-import nl.idgis.publisher.protocol.messages.Ack;
+import nl.idgis.publisher.provider.protocol.AbstractGetDatasetRequest;
 import nl.idgis.publisher.provider.protocol.GetVectorDataset;
 import nl.idgis.publisher.provider.protocol.VectorDatasetInfo;
 
-public class ProviderFetchVectorDatasetInitiator extends UntypedActor {
-	
-	private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+public class ProviderFetchVectorDatasetInitiator extends ProviderFetchDatasetInitiator<FetchVectorDataset, VectorDatasetInfo> {
 	
 	private final int GET_VECTOR_DATASET_MESSAGE_SIZE = 10;
 	
-	private final Duration timeout = Duration.create(15, TimeUnit.SECONDS);
-	
-	private final FetchVectorDataset request;
-	
-	private final ActorRef sender, receiver, provider;
-	
 	public ProviderFetchVectorDatasetInitiator(ActorRef sender, FetchVectorDataset request, ActorRef receiver, ActorRef provider) {
-		this.sender = sender;		
-		this.receiver = receiver;
-		this.provider = provider;
-		
-		this.request = request;
+		super(sender, request, receiver, provider);
 	}
 	
 	public static Props props(ActorRef sender, FetchVectorDataset request, ActorRef receiver, ActorRef provider) {
@@ -42,30 +23,13 @@ public class ProviderFetchVectorDatasetInitiator extends UntypedActor {
 	}
 	
 	@Override
-	public void preStart() throws Exception {
-		getContext().setReceiveTimeout(timeout);
+	protected StartImport startImport(VectorDatasetInfo vectorDatasetInfo) {
+		return new StartVectorImport(sender, vectorDatasetInfo.getNumberOfRecords());
 	}
-
+	
 	@Override
-	public void onReceive(Object msg) throws Exception {
-		if(msg instanceof ReceiveTimeout) {
-			log.error("timeout");
-			
-			getContext().stop(getSelf());
-		} else if(msg instanceof VectorDatasetInfo) {
-			log.debug("vector dataset info received");
-			
-			receiver.tell(new StartVectorImport(sender, ((VectorDatasetInfo)msg).getNumberOfRecords()), getSelf());
-		} else if(msg instanceof Ack) {
-			log.debug("receiver is ready");
-			
-			provider.tell(new GetVectorDataset(request.getId(), request.getColumns(), GET_VECTOR_DATASET_MESSAGE_SIZE), receiver);
-			
-			getContext().stop(getSelf());
-		} else {
-			unhandled(msg);
-		}
-		
+	protected AbstractGetDatasetRequest getDataset() {
+		return new GetVectorDataset(request.getId(), request.getColumns(), GET_VECTOR_DATASET_MESSAGE_SIZE);
 	}
-
+	
 }
