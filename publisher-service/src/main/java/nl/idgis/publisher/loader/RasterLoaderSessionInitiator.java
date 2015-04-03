555dcd07df91c110d92120c58ef50abe05622665
copierrj <reijer.copier@idgis.nl>
2015-04-03 15:25:46 +0200

    job status update fixed for raster import

--- publisher-service/src/main/java/nl/idgis/publisher/loader/RasterLoaderSessionInitiator.java
+++ publisher-service/src/main/java/nl/idgis/publisher/loader/RasterLoaderSessionInitiator.java
@@ -2,9 +2,14 @@ package nl.idgis.publisher.loader;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.japi.Procedure;
+
+import nl.idgis.publisher.domain.job.JobState;
 
 import nl.idgis.publisher.harvester.sources.messages.FetchRasterDataset;
+import nl.idgis.publisher.job.context.messages.UpdateJobState;
 import nl.idgis.publisher.job.manager.messages.RasterImportJobInfo;
+import nl.idgis.publisher.protocol.messages.Ack;
 
 public class RasterLoaderSessionInitiator extends AbstractLoaderSessionInitiator<RasterImportJobInfo> {
 	
@@ -22,12 +27,31 @@ public class RasterLoaderSessionInitiator extends AbstractLoaderSessionInitiator
 
 	@Override
 	protected void dataSourceReceived() throws Exception {
+		jobContext.tell(new UpdateJobState(JobState.STARTED), getSelf());		
+		become("storing started job state", waitingForJobStartedStored());
+	}
+
+	private void startLoaderSession() throws Exception {
 		startLoaderSession(new FetchRasterDataset(
 			importJob.getSourceDatasetId(), 
 			RasterLoaderSession.props(
 				getContext().parent(), // loader
 				importJob,
 				jobContext)));
+	}
+	
+	private Procedure<Object> waitingForJobStartedStored() {
+		return new Procedure<Object>() {
+
+			@Override
+			public void apply(Object msg) throws Exception {
+				if(msg instanceof Ack) {
+					startLoaderSession();
+				} else {
+					unhandled(msg);
+				}
+			}
+		};
 	}
 
 }
