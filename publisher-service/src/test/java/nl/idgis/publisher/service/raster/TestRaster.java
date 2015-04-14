7aa6c401b63b99c08fbe7c73086a580a3ae42cb8
copierrj <reijer.copier@idgis.nl>
2015-04-14 15:23:11 +0200

    test geotiff replaced
    source: http://download.osgeo.org/geotiff/samples/made_up/bogota.tif

--- publisher-service/src/test/java/nl/idgis/publisher/service/raster/TestRaster.java
+++ publisher-service/src/test/java/nl/idgis/publisher/service/raster/TestRaster.java
@@ -7,7 +7,7 @@ import java.net.URL;
 public class TestRaster {
 	
 	public static URL getRasterUrl() {
-		return TestRaster.class.getClassLoader().getResource("nl/idgis/publisher/service/raster/albers27.tif");
+		return TestRaster.class.getClassLoader().getResource("nl/idgis/publisher/service/raster/bogota.tif");
 	}
 
 	public static String getRasterFolder() throws IOException {
