16caa8bdfae3e786b3fccffb47af6b4837457b6b
copierrj <reijer.copier@idgis.nl>
2015-04-17 11:52:38 +0200

    retry implemented

---
+++ publisher-commons/src/main/java/nl/idgis/publisher/stream/messages/Retry.java
@@ -0,0 +1,14 @@
+package nl.idgis.publisher.stream.messages;
+
+import java.io.Serializable;
+
+public class Retry implements Serializable {
+
+	private static final long serialVersionUID = 15818275263641553L;
+
+	@Override
+	public String toString() {
+		return "Retry []";
+	}
+
+}
