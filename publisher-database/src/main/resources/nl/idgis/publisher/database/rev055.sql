74dea2c99bb8dab72b31d27584417877313a1b4c
copierrj <reijer.copier@idgis.nl>
2015-04-17 10:05:16 +0200

    ddl fix

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev055.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev055.sql
@@ -3,8 +3,8 @@ create table publisher.published_service_dataset(
 	service_id integer not null,
 	dataset_id integer not null,
 	
-	constraint published_service_dataset_service_id_fk foreign key(service_id) references service(id),
-	constraint published_service_dataset_dataset_id_fk foreign key(dataset_id) references dataset(id)
+	constraint published_service_dataset_service_id_fk foreign key(service_id) references publisher.service(id),
+	constraint published_service_dataset_dataset_id_fk foreign key(dataset_id) references publisher.dataset(id)
 );
 
 insert into publisher.version(id) values(55);
