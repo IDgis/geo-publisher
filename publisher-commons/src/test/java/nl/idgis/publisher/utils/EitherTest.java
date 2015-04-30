0cf04768dcec0eb9e34d382e3a69350ab7db5ead
copierrj <reijer.copier@idgis.nl>
2015-04-30 19:09:29 +0200

    Either utility class added

---
+++ publisher-commons/src/test/java/nl/idgis/publisher/utils/EitherTest.java
@@ -0,0 +1,62 @@
+package nl.idgis.publisher.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class EitherTest {
+
+	@Test
+	public void testLeft() {
+		Either<String, ?> either = Either.left("Hello, world!");
+		
+		Optional<String> optional = either.getLeft();
+		assertTrue(optional.isPresent());
+		assertEquals("Hello, world!", optional.get());
+		
+		assertFalse(either.getRight().isPresent());
+	}
+	
+	@Test
+	public void testRight() {
+		Either<?, String> either = Either.right("Hello, world!");
+		
+		Optional<String> optional = either.getRight();
+		assertTrue(optional.isPresent());
+		assertEquals("Hello, world!", optional.get());
+		
+		assertFalse(either.getLeft().isPresent());
+	}
+	
+	@Test
+	public void testMapLeft() {
+		Either<String, Integer> either = Either.left("Hello, world!");
+		assertEquals(Integer.valueOf(42), either.mapLeft(s -> 42));
+		assertEquals("Hello, world!", either.mapRight(o -> {
+			fail("mapper called");			
+			return "Not used";
+		}));
+	}
+	
+	@Test
+	public void testMapRight() {
+		Either<Integer, String> either = Either.right("Hello, world!");
+		assertEquals(Integer.valueOf(42), either.mapRight(s -> 42));
+		assertEquals("Hello, world!", either.mapLeft(o -> {
+			fail("mapper called");			
+			return "Not used";
+		}));
+	}
+	
+	@Test
+	public void testSwap() {
+		Either<?, String> either = Either.left("Hello, world!");
+		assertTrue(either.getLeft().isPresent());
+		assertTrue(either.swap().getRight().isPresent());
+	}
+}
