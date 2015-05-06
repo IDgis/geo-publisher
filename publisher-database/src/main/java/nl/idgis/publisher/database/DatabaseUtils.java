d290ef9957acf393ce3cc0d8c4f8f0828b6ec6eb
copierrj <reijer.copier@idgis.nl>
2015-05-06 13:37:27 +0200

    DatabaseUtils.consumeList is now able to skip over items

--- publisher-database/src/main/java/nl/idgis/publisher/database/DatabaseUtils.java
+++ publisher-database/src/main/java/nl/idgis/publisher/database/DatabaseUtils.java
@@ -11,19 +11,23 @@ import com.mysema.query.types.Expression;
 
 public class DatabaseUtils {
 
-	public static <T, U> List<T> consumeList(ListIterator<Tuple> listIterator, U id, Expression<U> idPath, Function1<Tuple, T> mapper) {
+	public static <T, U extends Comparable<U>> List<T> consumeList(ListIterator<Tuple> listIterator, U id, Expression<U> idPath, Function1<Tuple, T> mapper) {
 		List<T> retval = new ArrayList<>();
 		
 		for(; listIterator.hasNext();) {
 			Tuple tc = listIterator.next();
 			
-			U listId = tc.get(idPath);				
-			if(!listId.equals(id)) {
+			U listId = tc.get(idPath);
+			
+			int cmp = listId.compareTo(id);			
+			if(cmp > 0) {
 				listIterator.previous();
 				break;
+			} else if(cmp < 0) {
+				continue;
+			} else {			
+				retval.add(mapper.apply(tc));
 			}
-			
-			retval.add(mapper.apply(tc));
 		}
 		
 		return retval;
