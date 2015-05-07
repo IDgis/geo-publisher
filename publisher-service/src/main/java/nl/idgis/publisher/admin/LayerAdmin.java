1c105b8d2cfe4a96c87ad9813ebd185a06c24be9
copierrj <reijer.copier@idgis.nl>
2015-05-07 10:55:41 +0200

    obsolete index operation removed
    was used to populate the (now removed) column 'default_style'

--- publisher-service/src/main/java/nl/idgis/publisher/admin/LayerAdmin.java
+++ publisher-service/src/main/java/nl/idgis/publisher/admin/LayerAdmin.java
@@ -534,16 +534,14 @@ public class LayerAdmin extends LayerGroupCommonAdmin {
 									llNr -> {
 										// B. insert items of layerStyles	
 										return f.sequence(
-												StreamUtils.index(
-													layerStyles.stream()
-												)
-											    .map(indexed -> tx
+												layerStyles.stream()												
+											    .map(layerStyleId -> tx
 											.insert(layerStyle)
 											.columns(
 												layerStyle.layerId, 
 												layerStyle.styleId)
 											.select(new SQLSubQuery().from(style)
-												.where(style.identification.eq(indexed.getValue()))
+												.where(style.identification.eq(layerStyleId))
 												.list(
 													llId.get(),
 													style.id)
