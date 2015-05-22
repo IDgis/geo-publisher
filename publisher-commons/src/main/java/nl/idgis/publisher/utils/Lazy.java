4f5f97d4591e8f46e6cd2f5720bfafd63fbf5a16
copierrj <reijer.copier@idgis.nl>
2015-05-22 16:50:16 +0200

    Lazy is now Serializable

--- publisher-commons/src/main/java/nl/idgis/publisher/utils/Lazy.java
+++ publisher-commons/src/main/java/nl/idgis/publisher/utils/Lazy.java
@@ -1,14 +1,21 @@
 package nl.idgis.publisher.utils;
 
+import java.io.Serializable;
 import java.util.function.Supplier;
 
-public class Lazy<T> {
+public final class Lazy<T> implements Serializable {	
+
+	public static interface LazySupplier<T> extends Supplier<T>, Serializable {
+		
+	}
 	
-	private T t;
+	private static final long serialVersionUID = -8261891546560310755L;
+
+	private transient T t;
 	
-	private final Supplier<T> supplier;
+	private final LazySupplier<T> supplier;
 	
-	public Lazy(Supplier<T> supplier) {
+	public Lazy(LazySupplier<T> supplier) {
 		this.supplier = supplier;
 	}
 
--- publisher-commons/src/test/java/nl/idgis/publisher/utils/LazyTest.java
+++ publisher-commons/src/test/java/nl/idgis/publisher/utils/LazyTest.java
@@ -2,8 +2,13 @@ package nl.idgis.publisher.utils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import java.util.function.Supplier;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 import org.junit.Test;
 
@@ -16,7 +21,9 @@ public class LazyTest {
 	
 	@Test
 	public void testSupplyOnce() {
-		Lazy<Integer> lazyValue = new Lazy<>(new Supplier<Integer>() {
+		Lazy<Integer> lazyValue = new Lazy<>(new Lazy.LazySupplier<Integer>() {			
+			
+			private static final long serialVersionUID = -5183996451704320273L;
 			
 			private boolean supplied = false;
 			
@@ -31,5 +38,39 @@ public class LazyTest {
 		
 		assertEquals(Integer.valueOf(42), lazyValue.get());
 		assertEquals(Integer.valueOf(42), lazyValue.get());
+	}
+	
+	static class TestSerializable implements Serializable {		
+		
+		private static final long serialVersionUID = -9029456671476202582L;
+		
+		Lazy<Integer> lazyValue;
+		
+		TestSerializable(Lazy<Integer> lazyValue) {
+			this.lazyValue = lazyValue;
+		}
+	}
+	
+	@Test	
+	public void testSerializable() throws Exception {
+		Lazy<Integer> lazyValue = new Lazy<Integer>(() -> 42);
+		lazyValue.get();
+		
+		TestSerializable testSerializable = new TestSerializable(lazyValue);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
+		
+		oos.writeObject(testSerializable);		
+		oos.close();
+		
+		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+		ObjectInputStream ois = new ObjectInputStream(bais);
+		
+		Object obj = ois.readObject();
+		assertTrue(obj instanceof TestSerializable);
+		
+		Lazy<Integer> deserializedLazyValue = ((TestSerializable)obj).lazyValue;
+		assertEquals(Integer.valueOf(42), deserializedLazyValue.get());
 	}
 }
