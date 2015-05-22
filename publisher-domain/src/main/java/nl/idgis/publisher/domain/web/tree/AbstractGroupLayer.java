c3c40aed4abb2d53e223f3c1c8d2260797556327
copierrj <reijer.copier@idgis.nl>
2015-05-22 10:55:46 +0200

    Lazy used to cache confidential and importType

--- publisher-domain/src/main/java/nl/idgis/publisher/domain/web/tree/AbstractGroupLayer.java
+++ publisher-domain/src/main/java/nl/idgis/publisher/domain/web/tree/AbstractGroupLayer.java
@@ -1,5 +1,7 @@
 package nl.idgis.publisher.domain.web.tree;
 
+import nl.idgis.publisher.utils.Lazy;
+
 import java.io.Serializable;
 import java.util.List;
 import java.sql.Timestamp;
@@ -9,25 +11,38 @@ import java.util.Optional;
 public abstract class AbstractGroupLayer implements GroupLayer, Serializable {
 	
 	private static final long serialVersionUID = -5053415862522039499L;
+	
+	private final Lazy<Boolean> confidential;
+	
+	private final Lazy<Optional<Timestamp>> importTime;
+	
+	public AbstractGroupLayer() {
+		confidential = new Lazy<>(() -> {
+			List<LayerRef<? extends Layer>> childLayerRefs = getLayers();
+			for(final LayerRef<?> childLayerRef : childLayerRefs) {
+				if(childLayerRef.getLayer().isConfidential()) {
+					return true;
+				}
+			}
+			
+			return false;
+		});
+		
+		importTime = new Lazy<>(() ->
+			getLayers().stream()
+				.map(layerRef -> layerRef.getLayer().getImportTime())
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.max(Comparator.naturalOrder()));
+	}
 
 	@Override
 	public boolean isConfidential () {
-		List<LayerRef<? extends Layer>> childLayerRefs = getLayers();
-		for(final LayerRef<?> childLayerRef : childLayerRefs) {
-			if(childLayerRef.getLayer().isConfidential()) {
-				return true;
-			}
-		}
-		
-		return false;
+		return confidential.get();
 	}
 	
 	@Override
 	public Optional<Timestamp> getImportTime() {
-		return getLayers().stream()
-			.map(layerRef -> layerRef.getLayer().getImportTime())
-			.filter(Optional::isPresent)
-			.map(Optional::get)
-			.max(Comparator.naturalOrder());
+		return importTime.get();
 	}
 }
