1227fe5572139144d121609e1d362a868a2ee75a
copierrj <reijer.copier@idgis.nl>
2015-08-20 10:30:59 +0200

    error handling improved

--- publisher-service/src/main/java/nl/idgis/publisher/metadata/AbstractMetadataItemGenerator.java
+++ publisher-service/src/main/java/nl/idgis/publisher/metadata/AbstractMetadataItemGenerator.java
@@ -1,9 +1,12 @@
 package nl.idgis.publisher.metadata;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import akka.actor.ActorRef;
+import akka.actor.ReceiveTimeout;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -12,7 +15,9 @@ import scala.concurrent.duration.Duration;
 
 import nl.idgis.publisher.metadata.messages.MetadataItemInfo;
 import nl.idgis.publisher.metadata.messages.PutMetadata;
+import nl.idgis.publisher.protocol.messages.Failure;
 import nl.idgis.publisher.stream.messages.NextItem;
+import nl.idgis.publisher.utils.FutureUtils;
 
 public abstract class AbstractMetadataItemGenerator<T extends MetadataItemInfo, U extends PutMetadata> extends UntypedActor {
 	
@@ -22,6 +27,21 @@ public abstract class AbstractMetadataItemGenerator<T extends MetadataItemInfo, 
 	
 	protected final T itemInfo;
 	
+	private FutureUtils f;
+	
+	private static class GeneratorResult {
+		
+		private final Set<Failure> failures;
+		
+		GeneratorResult(Set<Failure> failures) {
+			this.failures = failures;
+		}
+		
+		Set<Failure> getFailures() {
+			return failures;
+		}
+	}
+	
 	AbstractMetadataItemGenerator(ActorRef metadataTarget, T itemInfo) {
 		this.metadataTarget = metadataTarget;
 		this.itemInfo = itemInfo;
@@ -30,6 +50,8 @@ public abstract class AbstractMetadataItemGenerator<T extends MetadataItemInfo, 
 	@Override
 	public final void preStart() throws Exception {
 		getContext().setReceiveTimeout(Duration.create(5, TimeUnit.SECONDS));
+		
+		f = new FutureUtils(getContext());
 	}
 	
 	protected abstract List<? extends PutMetadata> generateMetadata(MetadataDocument metadataDocument) throws Exception;
@@ -39,16 +61,58 @@ public abstract class AbstractMetadataItemGenerator<T extends MetadataItemInfo, 
 		log.debug("message received while generating metadata document: {}", msg);
 		
 		if(msg instanceof MetadataDocument) {
-			log.debug("metadata document received for item: {}", itemInfo.getId());
-			
-			generateMetadata((MetadataDocument)msg).forEach(putMetadata ->
-				metadataTarget.tell(putMetadata, getSelf()));
+			handleMetadataDocument((MetadataDocument)msg);
+		} else if(msg instanceof GeneratorResult) {
+			handleGeneratorResult((GeneratorResult)msg);
+		} else if(msg instanceof ReceiveTimeout) {
+			handleReceiveTimeout();
 		} else {
-			log.debug("terminating");
-			
-			getContext().parent().tell(new NextItem(), getSelf());
-			getContext().stop(getSelf());
+			unhandled(msg);
 		}
+	}
+
+	private void handleReceiveTimeout() {
+		log.error("timeout");
+		
+		stop();
+	}
+
+	private void handleGeneratorResult(Object msg) {
+		log.debug("generator finished");
+		
+		Set<Failure> failures = ((GeneratorResult) msg).getFailures(); 
+		if(failures.isEmpty()) {
+			log.debug("no failures");
+		} else {
+			failures.forEach(failure ->
+				log.warning("failure: {}", failure));
+		}
+		
+		stop();
+	}
+
+	private void handleMetadataDocument(MetadataDocument msg) throws Exception {
+		log.debug("metadata document received for item: {}", itemInfo.getId());
+		
+		generateMetadata(msg).stream()
+			.map(putMetadata -> f.ask(metadataTarget, putMetadata)
+				.exceptionally(t -> new Failure(t)))
+			.collect(f.collect()).thenAccept(results -> {
+				getSelf().tell(
+					new GeneratorResult(
+						results
+							.filter(result -> result instanceof Failure)
+							.map(result -> (Failure)result)
+							.collect(Collectors.toSet())),
+					getSelf());
+			});
+	}
+
+	private void stop() {
+		log.debug("terminating");
+		
+		getContext().parent().tell(new NextItem(), getSelf());
+		getContext().stop(getSelf());
 	}
 
 }
