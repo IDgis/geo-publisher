9bbf424df226b2d72ccd4d42f0ccaf09e70e207a
copierrj <reijer.copier@idgis.nl>
2015-08-26 14:00:36 +0200

    LRS awareness added to SDOGeometryConverter
    current solution ignores the LRS dimension.

---
+++ publisher-provider/publisher-provider-app/src/test/java/nl/idgis/publisher/provider/database/LRSTest.java
@@ -0,0 +1,42 @@
+package nl.idgis.publisher.provider.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.deegree.geometry.Geometry;
+import org.deegree.geometry.io.WKTWriter;
+import org.deegree.sqldialect.oracle.sdo.ExtendedSDOGeometryConverter;
+import org.deegree.sqldialect.oracle.sdo.SDOGeometryConverter;
+import org.junit.Test;
+
+import oracle.sql.STRUCT;
+
+@SuppressWarnings("deprecation")
+public class LRSTest {
+
+	// @Test
+	public void testLRS() throws Exception {
+		SDOGeometryConverter converter = new ExtendedSDOGeometryConverter();
+		//SDOGeometryConverter converter = new SDOGeometryConverter();
+		
+		Class.forName("oracle.jdbc.OracleDriver");		
+		Connection c = DriverManager.getConnection("jdbc:oracle:thin:@192.168.122.166:1521:GISBASIP", "raadpleger", "raadpleger");
+		
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT SHAPE FROM B2.DUURZAME_OV_ARC");
+		while(rs.next()) {
+			STRUCT struct = (STRUCT)rs.getObject(1);
+			
+			Geometry geometry = converter.toGeometry(struct, null);
+			
+			System.out.println(WKTWriter.write(geometry));		
+		}
+		rs.close();
+		
+		stmt.close();
+		
+		c.close();
+	}
+}
