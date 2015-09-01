abad9ef4806d3d817efe25e7d8e7af29c9daac82
copierrj <reijer.copier@idgis.nl>
2015-09-01 17:09:55 +0200

    MetadataTarget directory check improved
    was: is directory
    
    now: exists && is directory || not exists
    
    directory is created when not exists

--- publisher-service/src/test/java/nl/idgis/publisher/metadata/MetadataTargetTest.java
+++ publisher-service/src/test/java/nl/idgis/publisher/metadata/MetadataTargetTest.java
@@ -3,9 +3,15 @@ package nl.idgis.publisher.metadata;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,6 +32,14 @@ import nl.idgis.publisher.metadata.messages.PutServiceMetadata;
 import nl.idgis.publisher.protocol.messages.Ack;
 import nl.idgis.publisher.utils.FutureUtils;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.same;
+import static org.mockito.Matchers.anyVararg;
+
 public class MetadataTargetTest {
 	
 	ActorSystem actorSystem;
@@ -35,10 +49,6 @@ public class MetadataTargetTest {
 	FileSystem fileSystem;
 	
 	Path serviceMetadataDirectory, datasetMetadataDirectory;
-	
-	ActorRef metadataTarget;
-	
-	Set<Path> tempDirectories;
 
 	@Before
 	public void start() throws Exception {
@@ -46,15 +56,16 @@ public class MetadataTargetTest {
 		f = new FutureUtils(actorSystem);
 		
 		fileSystem = Jimfs.newFileSystem(Configuration.unix());
-		serviceMetadataDirectory = fileSystem.getPath("/service-metadata");
-		Files.createDirectory(serviceMetadataDirectory);
-		
+		serviceMetadataDirectory = fileSystem.getPath("/service-metadata");		
 		datasetMetadataDirectory = fileSystem.getPath("/dataset-metadata");
-		Files.createDirectory(datasetMetadataDirectory);
-		
-		tempDirectories = new HashSet<>();
-		
-		metadataTarget = actorSystem.actorOf(
+	}
+	
+	private ActorRef createMetadataTarget(Set<Path> tempDirectories) {
+		return createMetadataTarget(serviceMetadataDirectory, datasetMetadataDirectory, tempDirectories);
+	}
+
+	private ActorRef createMetadataTarget(Path serviceMetadataDirectory, Path datasetMetadataDirectory, Set<Path> tempDirectories) {
+		return actorSystem.actorOf(
 			MetadataTarget.props(
 				serviceMetadataDirectory, 
 				datasetMetadataDirectory,
@@ -76,7 +87,10 @@ public class MetadataTargetTest {
 	public void testPutServiceMetadata() throws Exception {
 		Path targetFile = serviceMetadataDirectory.resolve("serviceId.xml");
 		assertFalse(Files.exists(targetFile));
-				
+			
+		Set<Path> tempDirectories = new HashSet<>();
+		ActorRef metadataTarget = createMetadataTarget(tempDirectories);
+		
 		f.ask(metadataTarget, new BeginPutMetadata(), Ack.class).get();
 		
 		f.ask(metadataTarget, 
@@ -103,6 +117,9 @@ public class MetadataTargetTest {
 		Path targetFile = datasetMetadataDirectory.resolve("datasetId.xml");
 		assertFalse(Files.exists(targetFile));
 		
+		Set<Path> tempDirectories = new HashSet<>();
+		ActorRef metadataTarget = createMetadataTarget(tempDirectories);
+		
 		f.ask(metadataTarget, new BeginPutMetadata(), Ack.class).get();
 		
 		f.ask(metadataTarget, 
@@ -120,5 +137,48 @@ public class MetadataTargetTest {
 		
 		tempDirectories.forEach(tempDirectory ->
 			assertFalse(Files.exists(tempDirectory)));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testNotDirectory() throws IOException {
+		Files.createDirectories(datasetMetadataDirectory.getParent());
+		
+		Writer writer = Files.newBufferedWriter(datasetMetadataDirectory);
+		writer.write("Hello, world!");
+		writer.close();
+		
+		createMetadataTarget(new HashSet<>());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCannotCreate() throws Exception {
+		FileSystem fileSystem = mock(FileSystem.class);
+		
+		Path root = mock(Path.class);
+		when(root.getFileSystem()).thenReturn(fileSystem);
+		
+		FileSystemProvider provider = mock(FileSystemProvider.class);		
+		when(fileSystem.provider()).thenReturn(provider);
+		
+		// no paths exists except root
+		doThrow(new FileNotFoundException()).when(provider).checkAccess(any(Path.class), anyVararg());
+		doNothing().when(provider).checkAccess(same(root), anyVararg());
+		
+		Path serviceMetadataDirectory = mock(Path.class);
+		when(serviceMetadataDirectory.getFileSystem()).thenReturn(fileSystem);
+		when(serviceMetadataDirectory.toAbsolutePath()).thenReturn(serviceMetadataDirectory);
+		
+		Path relativeServiceMetadataDirectory = mock(Path.class);
+		when(root.relativize(same(serviceMetadataDirectory))).thenReturn(relativeServiceMetadataDirectory);		
+		when(root.resolve(relativeServiceMetadataDirectory)).thenReturn(serviceMetadataDirectory);
+		
+		// serviceMetadataDirectory is child of root
+		when(serviceMetadataDirectory.iterator()).thenReturn(Collections.singleton(relativeServiceMetadataDirectory).iterator());
+		when(relativeServiceMetadataDirectory.iterator()).thenReturn(Collections.singleton(relativeServiceMetadataDirectory).iterator());		
+		when(serviceMetadataDirectory.getParent()).thenReturn(root);				
+		
+		doThrow(new AccessDeniedException(null)).when(provider).createDirectory(same(serviceMetadataDirectory), anyVararg());
+		
+		createMetadataTarget(serviceMetadataDirectory, datasetMetadataDirectory, new HashSet<>());
 	}
 }
