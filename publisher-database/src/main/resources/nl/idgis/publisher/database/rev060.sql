9ec6eccb6701652aed1836a966844c67bc599871
copierrj <reijer.copier@idgis.nl>
2015-11-26 15:38:06 +0100

    initial content for publisher.dataset_view implemented

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev060.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev060.sql
@@ -9,6 +9,17 @@ create table publisher.dataset_view (
 	constraint dataset_view_index unique(dataset_id, index)
 );
 
+-- <PostgreSQL>
+insert into publisher.dataset_view(dataset_id, index, name, data_type)
+select dataset_id, index, name, data_type
+from publisher.dataset_column dc
+where exists (
+	select * from information_schema.tables t
+	join publisher.dataset d on d.identification = t.table_name
+	where t.table_schema = 'data' and dc.dataset_id = d.id
+);
+-- </PostgreSQL>
+
 create table publisher.dataset_copy (
 	dataset_id integer references publisher.dataset(id),
 	index integer not null,
