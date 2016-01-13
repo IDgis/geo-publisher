9d8f044145d388bca389cf9b570c274f1e144e43
copierrj <reijer.copier@idgis.nl>
2016-01-13 18:02:49 +0100

    publisher-database added

---
+++ buildSrc/src/main/groovy/ConcatSQLTask.groovy
@@ -0,0 +1,67 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+import java.io.File
+
+class ConcatSQLTask extends DefaultTask {
+
+	@Input
+	String inputDir
+	
+	@Input
+	String buildDir
+	
+	@Input
+	String destFileName
+	
+	@Input
+	String databaseType
+	
+	@TaskAction
+	def concat() {
+		def outputDir = new File(project.buildDir, buildDir)
+		
+		outputDir.mkdirs()
+		
+		def outputFile = new File(outputDir, destFileName)
+		if(outputFile.exists()) {
+			outputFile.delete();
+		}
+		
+		def writer = outputFile.newWriter()
+		
+		def ignorePattern = ~/.*?--.*?<(.*?)>/
+		
+		project.fileTree(dir: inputDir, includes: ['*.sql'])
+			.files
+			.toSorted { it.name }
+			.each {
+				writer.append("\n\n-- ${it.name}\n")
+				
+				boolean ignore = false
+				it.eachLine { line ->
+					def ignoreMatcher = ignorePattern.matcher(line.trim())
+					if(ignoreMatcher.matches()) {
+						def content = ignoreMatcher.group(1)
+						if(content.startsWith('/')) {
+							ignore = false
+						} else {												
+							if(content != databaseType) {
+								ignore = true
+							}
+						}
+					} else {
+						if(ignore) {
+							writer.append('-- ')
+						}
+					}
+					
+					writer.append(line)
+					writer.append('\n')
+				}
+			}
+			
+		writer.close()		
+	}
+}
