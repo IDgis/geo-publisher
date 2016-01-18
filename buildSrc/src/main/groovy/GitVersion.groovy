b249a99ae6e75996d108a7642df8acf3f4f73720
copierrj <reijer.copier@idgis.nl>
2016-01-18 13:51:09 +0100

    git version stuff moved to separate file

---
+++ buildSrc/src/main/groovy/GitVersion.groovy
@@ -0,0 +1,26 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import org.ajoberstar.grgit.Grgit
+
+class GitVersion implements Plugin<Project> {
+
+	void apply(Project project) {
+		def repo = Grgit.open(project.file('.'))
+		def tag = repo.describe()
+		def prefix = "${project.name}-"
+		
+		if(tag.startsWith(prefix)) {
+			project.version = tag.substring(prefix.length())
+		}
+		
+		def gitVersion = project.task('gitVersion') {
+			group = 'Help'
+			description = 'Displays version as derived from git describe'
+		}
+		
+		gitVersion << {
+			println "gitVersion: ${if(project.version) { project.version } else { 'undefined' } }"
+		}
+	}
+}
