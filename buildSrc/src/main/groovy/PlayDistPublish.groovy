ff5e55a2301ff4c3dd784da9710d10d60daa0358
copierrj <reijer.copier@idgis.nl>
2016-01-18 13:59:53 +0100

    play dist/publish stuff moved to separate file

---
+++ buildSrc/src/main/groovy/PlayDistPublish.groovy
@@ -0,0 +1,36 @@
+import org.gradle.api.Task
+
+import org.gradle.model.RuleSource
+import org.gradle.model.Mutate
+import org.gradle.model.ModelMap
+
+import org.gradle.play.distribution.PlayDistributionContainer
+
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.PublishingExtension
+
+class PlayDistPublish extends RuleSource {	
+	@Mutate void addAttributes(ModelMap<Task> tasks, PlayDistributionContainer dists, PublishingExtension publishing) {
+		dists.names.each {
+			def jarTask = tasks.get("create${it.capitalize()}DistributionJar")
+			
+			jarTask.configure {
+				manifest {
+					attributes("Implementation-Title": project.name)
+					if(project.version) {
+						attributes("Implementation-Version": project.version)
+					}
+				}
+			}
+			
+			def zipTask = tasks.get("create${it.capitalize()}Dist")
+			
+			publishing.publications {
+				mavenJava(MavenPublication) {
+					groupId 'nl.idgis.publisher'
+					artifact zipTask
+				}
+			}
+		}
+	}
+}
