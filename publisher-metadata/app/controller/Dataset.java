fc9ace34ec01faa28ee2d35f42edb96a60d84f23
copierrj <reijer.copier@idgis.nl>
2016-01-19 16:36:09 +0100

    properties and descriptions implemented

--- publisher-metadata/app/controller/Dataset.java
+++ publisher-metadata/app/controller/Dataset.java
@@ -11,7 +11,8 @@ import model.dav.ResourceDescription;
 import model.dav.ResourceProperties;
 
 import model.dav.DefaultResource;
-
+import model.dav.DefaultResourceDescription;
+import model.dav.DefaultResourceProperties;
 import nl.idgis.publisher.metadata.MetadataDocument;
 import nl.idgis.publisher.metadata.MetadataDocumentFactory;
 
@@ -33,6 +34,7 @@ import static nl.idgis.publisher.database.QSourceDatasetVersion.sourceDatasetVer
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static nl.idgis.publisher.database.QPublishedServiceDataset.publishedServiceDataset;
 
@@ -94,11 +96,49 @@ public class Dataset extends SimpleWebDAV {
 
 	@Override
 	public List<ResourceDescription> descriptions() {
-		return Collections.emptyList();
+		return q.withTransaction(tx -> {
+			return tx.query().from(dataset)
+				.join(sourceDataset).on(sourceDataset.id.eq(dataset.sourceDatasetId))
+				.join(sourceDatasetMetadata).on(sourceDatasetMetadata.sourceDatasetId.eq(sourceDataset.id))
+				.join(sourceDatasetVersion).on(sourceDatasetVersion.sourceDatasetId.eq(sourceDataset.id))
+				.where(sourceDatasetVersion.id.in(new SQLSubQuery().from(sourceDatasetVersion)
+					.where(sourceDatasetVersion.sourceDatasetId.eq(sourceDataset.id))
+					.list(sourceDatasetVersion.id.max())))
+				.where(sourceDatasetVersion.confidential.isFalse())
+				.list(dataset.metadataFileIdentification).stream()
+					.map(id -> {
+						ResourceProperties properties = new DefaultResourceProperties();
+						
+						return new DefaultResourceDescription(id + ".xml", properties); 
+					})
+					.collect(Collectors.toList());
+		});
 	}
 
 	@Override
 	public Optional<ResourceProperties> properties(String name) {
-		return Optional.empty();
+		if(name.toLowerCase().endsWith(".xml")) {
+			String id = name.substring(0, name.length() - 4);
+			
+			return q.withTransaction(tx -> {
+				if(tx.query().from(dataset)
+					.join(sourceDataset).on(sourceDataset.id.eq(dataset.sourceDatasetId))
+					.join(sourceDatasetMetadata).on(sourceDatasetMetadata.sourceDatasetId.eq(sourceDataset.id))
+					.join(sourceDatasetVersion).on(sourceDatasetVersion.sourceDatasetId.eq(sourceDataset.id))
+					.where(sourceDatasetVersion.id.in(new SQLSubQuery().from(sourceDatasetVersion)
+						.where(sourceDatasetVersion.sourceDatasetId.eq(sourceDataset.id))
+						.list(sourceDatasetVersion.id.max())))
+					.where(sourceDatasetVersion.confidential.isFalse())
+					.where(dataset.metadataFileIdentification.eq(id))
+					.exists()) {
+						
+					return Optional.<ResourceProperties>of(new DefaultResourceProperties());
+				} else {
+					return Optional.<ResourceProperties>empty();
+				}
+			});
+		} else {
+			return Optional.empty();
+		}
 	}
 }
