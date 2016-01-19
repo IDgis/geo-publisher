eb15a86ad1ddab2f578bcfb6b509579ecc07c7ef
copierrj <reijer.copier@idgis.nl>
2016-01-19 14:52:44 +0100

    on delete cascade added

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev061.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev061.sql
@@ -1,6 +1,6 @@
 
 create table publisher.source_dataset_metadata(
-	source_dataset_id integer references publisher.source_dataset(id),
+	source_dataset_id integer references publisher.source_dataset(id) on delete cascade,
 	document bytea not null,
 	
 	unique(source_dataset_id)
