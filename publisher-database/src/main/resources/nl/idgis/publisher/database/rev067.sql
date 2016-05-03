f0fb07eb9b915e8ba935c25ca5ce29ef9f832114
copierrj <reijer.copier@idgis.nl>
2016-05-03 11:37:36 +0200

    missing not null constraints added

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev067.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev067.sql
@@ -1,8 +1,8 @@
 
 create table publisher.source_dataset_metadata_attachment(
 	id serial primary key,
-	source_dataset_id integer references publisher.source_dataset(id) on delete cascade,	
-	identification text,
+	source_dataset_id integer not null references publisher.source_dataset(id) on delete cascade,
+	identification text not null,
 	content_type text,
 	content_disposition text,
 	content bytea not null
