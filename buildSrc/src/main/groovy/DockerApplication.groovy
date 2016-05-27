3f1f5e635e4b3afecf021671ee5296ebd10edb8d
copierrj <reijer.copier@idgis.nl>
2016-05-27 14:15:06 +0200

    DockerApplication plugin implemented

---
+++ buildSrc/src/main/groovy/DockerApplication.groovy
@@ -0,0 +1,63 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.plugins.ApplicationPlugin
+
+import com.bmuschko.gradle.docker.DockerRemoteApiPlugin
+import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
+
+class DockerApplication implements Plugin<Project> {
+
+	void apply(Project project) {
+		project.pluginManager.apply(ApplicationPlugin)
+		project.pluginManager.apply(DockerRemoteApiPlugin)
+		
+		def env = System.env
+		project.docker {
+			if(env.containsKey('DOCKER_HOST')) {
+				url = "$env.DOCKER_HOST"
+
+				if(env.containsKey('DOCKER_TLS_VERIFY')) {
+					url = url.replace('tcp', 'https')
+				} else {
+					url = url.replace('tcp', 'http')
+				}
+
+				if(env.containsKey('DOCKER_CERT_PATH')) {
+					certPath = project.file "$env.DOCKER_CERT_PATH"
+				}
+			} else {
+				url = "http://${dockerHost}:2375"
+			}
+		}
+		
+		def copyTar = project.tasks.create('copyTar', Copy, {
+			def distTar = project.tasks.getByName('distTar')
+		
+			dependsOn distTar
+			
+			from project.tarTree(distTar.archivePath)
+			into "${project.buildDir}/docker"
+		})
+		
+		def createDockerfile = project.tasks.create('createDockerfile', Dockerfile, {
+			dependsOn copyTar
+			
+			destFile = project.file("${project.buildDir}/docker/Dockerfile")
+			from 'java'
+			copyFile "${project.name}-${project.version}", '/opt'
+			runCommand "chmod u+x /opt/bin/${project.name}"
+			defaultCommand "/opt/bin/${project.name}"
+		})
+		
+		def buildImage = project.tasks.create('buildImage', DockerBuildImage, {
+			def moduleName = project.name.substring(project.name.indexOf('-') + 1)
+		
+			dependsOn createDockerfile
+			
+			inputDir = project.file("${project.buildDir}/docker")
+			tag = "idgis/geopublisher_${moduleName}:${project.version}"
+		})
+	}
+}
