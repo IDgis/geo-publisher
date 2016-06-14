2741838c6876c51fd892f524f3673692c4087142
copierrj <reijer.copier@idgis.nl>
2016-06-14 16:11:18 +0200

    quotes removed

--- publisher-service/src/main/docker/start.sh
+++ publisher-service/src/main/docker/start.sh
@@ -12,8 +12,8 @@ else
 		-keyalg RSA \
 		-dname 'cn=IDgis Developers, ou=None, L=Rijssen, ST=Overijssel, o=IDgis bv, c=NL' \
 		-keystore $SSL_PRIVATE_KEYSTORE \
-		-storepass '$SSL_PRIVATE_KEYSTORE_PASSWORD' \
-		-keypass '$SSL_PRIVATE_KEYSTORE_PASSWORD' \
+		-storepass $SSL_PRIVATE_KEYSTORE_PASSWORD \
+		-keypass $SSL_PRIVATE_KEYSTORE_PASSWORD \
 		-alias 'private'
 	echo Private keystore created
 fi
@@ -28,8 +28,8 @@ else
 		-keyalg RSA \
 		-dname 'cn=IDgis Developers, ou=None, L=Rijssen, ST=Overijssel, o=IDgis bv, c=NL' \
 		-keystore $SSL_TRUSTED_KEYSTORE \
-		-storepass '$SSL_TRUSTED_KEYSTORE_PASSWORD' \
-		-keypass '$SSL_TRUSTED_KEYSTORE_PASSWORD' \
+		-storepass $SSL_TRUSTED_KEYSTORE_PASSWORD \
+		-keypass $SSL_TRUSTED_KEYSTORE_PASSWORD \
 		-alias 'trusted'
 	echo Trusted keystore created
 fi
