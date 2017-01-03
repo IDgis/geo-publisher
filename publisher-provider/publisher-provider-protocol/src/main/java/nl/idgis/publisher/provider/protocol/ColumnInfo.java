4caafaaa961da1a49502a5323adc9610ceb7a030
copierrj <reijer.copier@idgis.nl>
2017-01-03 12:26:42 +0100

    add alias attribute to ColumnInfo

--- publisher-provider/publisher-provider-protocol/src/main/java/nl/idgis/publisher/provider/protocol/ColumnInfo.java
+++ publisher-provider/publisher-provider-protocol/src/main/java/nl/idgis/publisher/provider/protocol/ColumnInfo.java
@@ -1,6 +1,8 @@
 package nl.idgis.publisher.provider.protocol;
 
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.Optional;
 
 import nl.idgis.publisher.domain.service.Type;
 
@@ -11,21 +13,25 @@ import nl.idgis.publisher.domain.service.Type;
  *
  */
 public class ColumnInfo implements Serializable {
-	
-	private static final long serialVersionUID = 4496684640742666661L;
-	
+
+	private static final long serialVersionUID = 5239530430444929445L;
+
 	private final String name;
 	
 	private final Type type;
+	
+	private final String alias;
 	
 	/**
 	 * Creates a column.
 	 * @param name the column name.
 	 * @param type the data type.
+	 * @param alias an arbitrary string to denote the meaning of this column. May be null.
 	 */
-	public ColumnInfo(String name, Type type) {
-		this.name = name;
-		this.type = type;
+	public ColumnInfo(String name, Type type, String alias) {
+		this.name = Objects.requireNonNull(name, "name should not be null");
+		this.type = Objects.requireNonNull(type, "type should not be null");
+		this.alias = alias;
 	}
 
 	/**
@@ -43,9 +49,17 @@ public class ColumnInfo implements Serializable {
 	public Type getType() {
 		return type;
 	}
+	
+	/**
+	 * 
+	 * @return the column alias
+	 */
+	public Optional<String> getAlias() {
+		return Optional.ofNullable(alias);
+	}
 
 	@Override
 	public String toString() {
-		return "ColumnInfo [name=" + name + ", type=" + type + "]";
+		return "ColumnInfo [name=" + name + ", type=" + type + ", alias=" + alias + "]";
 	}
 }
