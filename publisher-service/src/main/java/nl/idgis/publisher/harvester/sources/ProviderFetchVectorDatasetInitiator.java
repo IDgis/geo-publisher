df2dc64dd5edbb64cca95e5b50e216f405f7da7c
copierrj <reijer.copier@idgis.nl>
2017-01-06 10:53:30 +0100

    message size change reverted

--- publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderFetchVectorDatasetInitiator.java
+++ publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderFetchVectorDatasetInitiator.java
@@ -12,8 +12,7 @@ import nl.idgis.publisher.provider.protocol.VectorDatasetInfo;
 
 public class ProviderFetchVectorDatasetInitiator extends ProviderFetchDatasetInitiator<FetchVectorDataset, VectorDatasetInfo> {
 	
-	//private final int GET_VECTOR_DATASET_MESSAGE_SIZE = 100;
-	private final int GET_VECTOR_DATASET_MESSAGE_SIZE = 1;
+	private final int GET_VECTOR_DATASET_MESSAGE_SIZE = 100;
 	
 	public ProviderFetchVectorDatasetInitiator(ActorRef sender, FetchVectorDataset request, ActorRef receiver, ActorRef provider) {
 		super(sender, request, receiver, provider);
