ac0def7db8f3591c05e6b608d38b45cabcec08f0
Sandro Neumann <sandro.neumann@idgis.nl>
2017-05-02 14:09:41 +0200

    fix test

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev076.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev076.sql
@@ -3,7 +3,7 @@ create table publisher.harvest_notification(
 	id serial primary key,
 	notification_type varchar(80) not null,
 	create_time timestamp not null default now(),
-	source_dataset_id integer not null references publisher.source_dataset(id),
+	source_dataset_id integer not null references publisher.source_dataset(id) on delete cascade,
 	source_dataset_version_id integer not null references publisher.source_dataset_version(id) on delete cascade,
 	done boolean not null
 );
