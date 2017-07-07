8c180dd657eb963e5150874ff65fa384f644f089
Sandro Neumann <sandro.neumann@idgis.nl>
2017-07-07 15:55:12 +0200

    added sld scheme validation separation for 1.0.0 and 1.1.0

--- publisher-schemas/src/main/java/nl/idgis/publisher/schemas/SchemaRef.java
+++ publisher-schemas/src/main/java/nl/idgis/publisher/schemas/SchemaRef.java
@@ -2,7 +2,8 @@ package nl.idgis.publisher.schemas;
 
 public enum SchemaRef {
 
-	GEOSERVER_SLD("/schemas/geoserver/sld/StyledLayerDescriptor.xsd");
+	GEOSERVER_SLD_1_0_0("/schemas/geoserver/sld/StyledLayerDescriptor.xsd"),
+	GEOSERVER_SLD_1_1_0("/StyledLayerDescriptor-1.1.0.xsd");
 	
 	private final String path;
 	
