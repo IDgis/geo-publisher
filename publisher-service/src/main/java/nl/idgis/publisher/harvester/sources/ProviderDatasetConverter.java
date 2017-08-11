7585dbb74c43804c2dcea9f48349d90ee931804e
copierrj <reijer.copier@idgis.nl>
2017-08-11 10:43:13 +0200

    Improve metadata parsing and processing
    Correct default values for confidential and metadataConfidential and add
    additional logging.

--- publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderDatasetConverter.java
+++ publisher-service/src/main/java/nl/idgis/publisher/harvester/sources/ProviderDatasetConverter.java
@@ -37,6 +37,7 @@ import nl.idgis.publisher.provider.protocol.TableInfo;
 import nl.idgis.publisher.provider.protocol.VectorDatasetInfo;
 import nl.idgis.publisher.stream.StreamConverter;
 import nl.idgis.publisher.stream.messages.Start;
+import nl.idgis.publisher.utils.ConfigUtils;
 
 public class ProviderDatasetConverter extends StreamConverter {
 	
@@ -48,11 +49,26 @@ public class ProviderDatasetConverter extends StreamConverter {
 	
 	private final MetadataDocumentFactory mdf;
 	
-	private final Config harvesterConfig;
+	private final String confidentialPath;
+	
+	private final String dataPublicValue;
+	
+	private final String metadataPublicValue;
+	
+	private final String wmsOnlyValue;
 	
 	public ProviderDatasetConverter(ActorRef provider, Config harvesterConfig) throws Exception {
 		this.provider = provider;
-		this.harvesterConfig = harvesterConfig;
+		
+		confidentialPath = ConfigUtils.getOptionalString(harvesterConfig, "confidentialPath");
+		dataPublicValue = ConfigUtils.getOptionalString(harvesterConfig, "dataPublicValue");
+		metadataPublicValue = ConfigUtils.getOptionalString(harvesterConfig, "metadataPublicValue");
+		wmsOnlyValue = ConfigUtils.getOptionalString(harvesterConfig,"wmsOnlyValue");
+		
+		log.debug("confidentialPath: {}", confidentialPath);
+		log.debug("dataPublicValue: {}", dataPublicValue);
+		log.debug("metadataPublicValue: {}", metadataPublicValue);
+		log.debug("wmsOnlyValue: {}", wmsOnlyValue);
 		
 		Set<AttachmentType> attachmentTypes = new HashSet<>();
 		attachmentTypes.add(AttachmentType.METADATA);
@@ -82,19 +98,23 @@ public class ProviderDatasetConverter extends StreamConverter {
 			String identification = datasetInfo.getIdentification();
 			String title = datasetInfo.getTitle();
 			String alternateTitle = datasetInfo.getAlternateTitle();
-			String categoryId = datasetInfo.getCategoryId();			
+			String categoryId = datasetInfo.getCategoryId();
 			Date revisionDate = datasetInfo.getRevisionDate();
 			Set<Log> logs = datasetInfo.getLogs();
+			
+			log.debug("Converting provider dataset info for dataset {}", identification);
 			
 			Map<AttachmentType, Attachment> attachments = datasetInfo.getAttachments().stream()
 				.collect(Collectors.toMap(
 					Attachment::getAttachmentType,
 					Function.identity()));
 			
-			boolean confidential = false;
-			boolean metadataConfidential = false;
-			boolean wmsOnly = false;
 			MetadataDocument metadata;
+			
+			boolean confidential = true;
+			boolean metadataConfidential = true;
+			boolean wmsOnly = false;
+			
 			if(attachments.containsKey(AttachmentType.METADATA)) {
 				Attachment attachment = attachments.get(AttachmentType.METADATA);
 				Object content = attachment.getContent();
@@ -102,29 +122,46 @@ public class ProviderDatasetConverter extends StreamConverter {
 					try {
 						metadata = mdf.parseDocument((byte[])attachment.getContent());
 						
-						String confidentialsElement = harvesterConfig.getString("confidentialPath");
-						
-						List<String> useLimitations = metadata.getConfidentialElements(confidentialsElement);
-						
-						confidential = !useLimitations.contains(harvesterConfig.getString("dataPublicValue"));
-						metadataConfidential = !useLimitations.contains(harvesterConfig.getString("metadataPublicValue"));
-						wmsOnly = useLimitations.contains(harvesterConfig.getString("wmsOnlyValue"));
-						
-						log.debug("data confidential: " + confidential);
-						log.debug("metadata confidential: " + metadataConfidential);
+						try {
+							if(confidentialPath != null) {
+								List<String> useLimitations = metadata.getConfidentialElements(confidentialPath);
+								
+								log.debug("useLimitations: {}", useLimitations);
+								
+								if(dataPublicValue != null) {
+									confidential = !useLimitations.contains(dataPublicValue);
+									log.debug("data confidential: {}", confidential);
+								}
+								
+								if(metadataPublicValue != null) {
+									metadataConfidential = !useLimitations.contains(metadataPublicValue);
+									log.debug("metadata confidential: {}", metadataConfidential);
+								}
+								
+								if(wmsOnlyValue != null) {
+									wmsOnly = useLimitations.contains(wmsOnlyValue);
+									log.debug("wmsOnly: {}", wmsOnly);
+								}
+							} 
+						} catch(Exception e) {
+							log.error(e, "Couldn't process metadata");
+						}
 					} catch(Exception e) {
+						log.error(e, "Couldn't parse metadata");
 						metadata = null;
 					}
 				} else {
+					log.warning("Unexpected metadata object: {}", content);
 					metadata = null;
 				}
 			} else {
+				log.warning("No metadata received");
 				metadata = null;
 			}
 			
 			final Dataset dataset;
 			if(datasetInfo instanceof VectorDatasetInfo) {
-				log.debug("vector dataset info: " + datasetInfo);
+				log.debug("vector dataset info: {}", datasetInfo);
 				
 				VectorDatasetInfo vectorDatasetInfo = (VectorDatasetInfo)datasetInfo;
 				TableInfo tableInfo = vectorDatasetInfo.getTableInfo();
