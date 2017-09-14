b95ba14e6df787fdebe9d02b295d547c891912f2
Michiel Faber <michiel.faber@idgis.nl>
2017-09-14 09:22:26 +0200

    Basic setup of generating the JSON file.

--- publisher-metadata/app/controllers/DatasetMetadataDCAT.java
+++ publisher-metadata/app/controllers/DatasetMetadataDCAT.java
@@ -1,26 +1,95 @@
 package controllers;
 
-import javax.inject.Inject;
-
+import play.mvc.*;
 import play.libs.Json;
 
-public class DatasetMetadataDCAT {
+import java.util.HashMap;
 
-	//private final DatasetQueryBuilder dqb;
+import javax.inject.Inject;
+
+import com.mysema.query.Tuple;
+
+public class DatasetMetadataDCAT extends Controller{
+
+	/*
+	 * Gebruik alleen gepubiceerde datasets
+	 * 1 bestand met alle datasets erin.
+	 * Voorbeeld: https://geoportaal-ddh.opendata.arcgis.com/data.json
+	 * Referentie: https://www.w3.org/TR/vocab-dcat/
+	 * ISO 19115 -> DCAT Mapping: https://www.w3.org/2015/spatial/wiki/ISO_19115_-_DCAT_-_Schema.org_mapping
+	 * CKAN - DCAT Mapping: https://github.com/ckan/ckanext-dcat#rdf-dcat-to-ckan-dataset-mapping
+	 * 
+	 */
+
+	private HashMap<String, Object> dcatResult = new HashMap<String, Object>();
 	
-	DCAT dcatResult = new DCAT()
-	
-	/**
-	 @Inject
-	 
-	public DatasetMetadataDCAT(DatasetQueryBuilder dqb) throws Exception {
-		this.dqb = dqb;
+	/* The first four items on a DCAT/JSON  are standard. 
+	 * They are filled in here
+	 * See: https://project-open-data.cio.gov/v1.1/schema
+	 * and: https://geoportaal-ddh.opendata.arcgis.com/data.json.
+	 */
+	private void setHeader() {
+		String conformsTo = "https://project-open-data.cio.gov/v1.1/schema";
+		String context = "https://project-open-dat�1/schema/catalog.jsonld";
+		String type = "dcat:Catalog";
+		String describedBy = "https://project-open-data.cio.gov/v1.1/schema/catalog.json";
+		
+		dcatResult.put("conformsTo", conformsTo);
+		dcatResult.put("context", context);
+		dcatResult.put("type", type);
+		dcatResult.put("describedBy", describedBy);	
+		
 	}
-	*//
+	
+	
+	
+	private void populateDcatResult() {
+		// Loop over all dataset to generate correct dcat metadata
+		HashMap<String, Object> datasetsDcat = new HashMap<String, Object>();
+		//for(Tuple dataset : datasets) {
+			int i = 0;
+			
+			//String identification = dataset.get(dataset.identification);
+			String identification = "123445-1234-1234-123456789012";
+			datasetsDcat.put(String.valueOf(i), mapDcat(getDatasetMetadata(identification)));
+			i++;
+		//}
+		
+		// Add hashmap with all datasets to our result
+		dcatResult.put("dataset", datasetsDcat);
+	}
+
+	private Dcat getDatasetMetadata(String identification) {
+		// See /publisher-commons/src/main/java/nl/idgis/publisher/metadata/MetadataDocument.java
+		
+		return new Dcat("public", identification, "myTitle", "Opendata Portaal geoinformatie Den Haag");
+	}
+	
+	private HashMap<String, Object> mapDcat(Dcat dataset) {
+	
+		HashMap<String, Object> resultDataset = new HashMap<String, Object>();
+		
+		// Get the publisher
+		HashMap<String, String> publisher = new HashMap<String, String>();
+		publisher.put("name", dataset.getPublisherName());
+		resultDataset.put("publisher", publisher);
+		
+		// Get the contactPoint
+		
+		// Get all other info
+		resultDataset.put("title", dataset.getTitle());
+		
+		
+		
+		// Get acceslevel
+		resultDataset.put("accesslevel", dataset.getAccessLevel());
+		
+		return resultDataset;
+	}
+	
 	public Result index() {
-		dcatResult.setName("M. Faber")
-		return ok(Json.toJson(dcatResult))
+		setHeader();
+		populateDcatResult();
+		return ok(Json.toJson(dcatResult));
 	}
-	
-	
 }
---
+++ publisher-metadata/app/controllers/Dcat.java
@@ -0,0 +1,135 @@
+package controllers;
+import java.util.List;
+
+public class Dcat {
+
+	private final String typeDcatDataset =	"dcat:Dataset";
+	private String identifier;
+	private String title;
+	private String description;
+	private String issued;
+	private String modified;
+	private String publisherName;
+	private String accessLevel;	
+	private String landingPage;
+	private String license;
+	private String spatial;
+	// private String webService;
+	
+	private List<String> keyword;	
+	private List<String> theme;	
+	
+	private final String typeDcatContact =	"vcard:Contact";
+	private String contactPointFn;
+	private String contactPointHasEmail;
+	
+		/*
+	 distribution	[6]
+	 */
+		
+	public Dcat(String accessLevel, String identifier, String title, String publisherName) {
+		this.identifier = identifier;
+		this.title = title;
+		// description
+		// issued;
+		// modified;
+		this.publisherName = publisherName;
+		this.accessLevel = accessLevel;
+		//landingPage
+		//license
+		//spatial
+		
+		//keyword
+		//theme
+		
+		//contactPointFn
+		//contactPointHasEmail
+		}
+	
+	
+		
+	public Dcat(String description, List<String> theme, String landingPage, String publisherName, String issued,
+				String modified, String contactPointFn, String identifier, String title, String accessLevel,
+				String contactPointHasEmail, String spatial, String license, List<String> keyword) {
+			this.description = description;
+			this.theme = theme;
+			this.landingPage = landingPage;
+			this.publisherName = publisherName;
+			this.issued = issued;
+			this.modified = modified;
+			this.contactPointFn = contactPointFn;
+			this.identifier = identifier;
+			this.title = title;
+			this.accessLevel = accessLevel;
+			this.contactPointHasEmail = contactPointHasEmail;
+			this.spatial = spatial;
+			this.license = license;
+			this.keyword = keyword;
+		}
+
+
+
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public String getTypeDcatDataset() {
+		return typeDcatDataset;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getIssued() {
+		return issued;
+	}
+
+	public String getModified() {
+		return modified;
+	}
+
+	public String getPublisherName() {
+		return publisherName;
+	}
+
+	public String getLandingPage() {
+		return landingPage;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public String getSpatial() {
+		return spatial;
+	}
+
+	public List<String> getKeyword() {
+		return keyword;
+	}
+
+	public List<String> getTheme() {
+		return theme;
+	}
+
+	public String getTypeDcatContact() {
+		return typeDcatContact;
+	}
+
+	public String getContactPointFn() {
+		return contactPointFn;
+	}
+
+	public String getContactPointHasEmail() {
+		return contactPointHasEmail;
+	}
+}
