d0be646276dc0a35591f3e2f2a49fbf06d90a3c9
Sandro Neumann <sandro.neumann@idgis.nl>
2017-09-22 17:04:52 +0200

    added raster url display & raster url prefix to md config

--- publisher-metadata/app/util/MetadataConfig.java
+++ publisher-metadata/app/util/MetadataConfig.java
@@ -22,9 +22,13 @@ public class MetadataConfig {
 	
 	private final Boolean downloadUrlDisplay;
 	
+	private final Boolean rasterUrlDisplay;
+	
 	private final String downloadUrlPrefixExternal;
 	
 	private final String downloadUrlPrefixInternal;
+	
+	private final String rasterUrlPrefix;
 	
 	private final String viewerUrlPublicPrefix;
 	
@@ -53,11 +57,15 @@ public class MetadataConfig {
 		
 		downloadUrlDisplay = metadata.getBoolean("download-url-display");
 		
+		rasterUrlDisplay = metadata.getBoolean("raster-url-display");
+		
 		metadataUrlPrefix = "http://" + host + path + (path.endsWith("/") ? "" : "/") + "metadata/";
 		
 		downloadUrlPrefixExternal = metadata.getString("download-url-prefix-external");
 		
 		downloadUrlPrefixInternal = metadata.getString("download-url-prefix-internal");
+		
+		rasterUrlPrefix = metadata.getString("raster-url-prefix");
 		
 		viewerUrlPublicPrefix = metadata.getString("viewer-url-prefix-public");
 		
@@ -105,6 +113,10 @@ public class MetadataConfig {
 	public Boolean getDownloadUrlDisplay() {
 		return downloadUrlDisplay;
 	}
+	
+	public Boolean getRasterUrlDisplay() {
+		return rasterUrlDisplay;
+	}
 
 	public Optional<String> getDownloadUrlPrefixExternal() {
 		return Optional.ofNullable(downloadUrlPrefixExternal);
@@ -113,7 +125,11 @@ public class MetadataConfig {
 	public Optional<String> getDownloadUrlPrefixInternal() {
 		return Optional.ofNullable(downloadUrlPrefixInternal);
 	}
-
+	
+	public Optional<String> getRasterUrlPrefix() {
+		return Optional.ofNullable(rasterUrlPrefix);
+	}
+	
 	public Optional<String> getViewerUrlWmsOnlyPrefix() {
 		return Optional.ofNullable(viewerUrlWmsOnlyPrefix);
 	}
