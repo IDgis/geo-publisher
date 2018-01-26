760c96530d54b6e67b7537b5b41b451da1a2ec6f
copierrj <reijer.copier@idgis.nl>
2018-01-26 12:47:55 +0100

    Add support for http redirect to WMSTest

--- publisher-monitor/src/main/java/nl/idgis/publisher/monitor/WMSException.java
+++ publisher-monitor/src/main/java/nl/idgis/publisher/monitor/WMSException.java
@@ -2,14 +2,18 @@ package nl.idgis.publisher.monitor;
 
 public class WMSException extends Exception {	
 	
-	private static final long serialVersionUID = -2923377660306258566L;
+	private static final long serialVersionUID = 347362050740853678L;
 	
 	private final int responseCode;
-
-	public WMSException(int responseCode) {
-		super("unexpected response code: " + responseCode);
+	
+	public WMSException(int responseCode, String message) {
+		super((message == null ? "" : message + " ") +  "HTTP status: " + responseCode);
 		
 		this.responseCode = responseCode;
+	}
+
+	public WMSException(int responseCode) {
+		this(responseCode, null);
 	}
 	
 	public int getResponseCode() {
