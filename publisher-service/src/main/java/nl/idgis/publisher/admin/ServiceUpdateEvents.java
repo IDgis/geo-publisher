9c5140a8ef209e735106fc64458fba45187ca75a
Reijer Copier <reijer.copier@idgis.nl>
2019-08-02 17:33:55 +0200

    Send event on service put

--- publisher-service/src/main/java/nl/idgis/publisher/admin/ServiceUpdateEvents.java
+++ publisher-service/src/main/java/nl/idgis/publisher/admin/ServiceUpdateEvents.java
@@ -1,11 +1,16 @@
 package nl.idgis.publisher.admin;
 
+import nl.idgis.publisher.domain.web.Service;
+
 import akka.actor.ActorRef;
 import akka.actor.Props;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class ServiceUpdateEvents extends AbstractAdmin {
 
@@ -14,6 +19,8 @@ public class ServiceUpdateEvents extends AbstractAdmin {
     private Channel channel;
     
     private static final String EXCHANGE_NAME = "pub.geoserver.staging.updates";
+    
+    private final ObjectMapper om = new ObjectMapper();
 
 	public ServiceUpdateEvents(ActorRef database) {
 		super(database);
@@ -21,6 +28,18 @@ public class ServiceUpdateEvents extends AbstractAdmin {
 	
 	public static Props props(ActorRef database) {
 		return Props.create(ServiceUpdateEvents.class, database);
+	}
+	
+	private void sendUpdateEvent(String type, String name) {
+        try {	
+            ObjectNode message = om.createObjectNode();
+            message.put("type", type);
+            message.put("name", name);
+            
+            channel.basicPublish(EXCHANGE_NAME, "", null, om.writeValueAsBytes(message));
+        } catch(Exception e) {
+            throw new RuntimeException("Failed to send message to RabbitMQ", e);
+        }
 	}
 	
 	@Override
@@ -36,5 +55,7 @@ public class ServiceUpdateEvents extends AbstractAdmin {
         } catch(Exception e) {
             throw new RuntimeException("Failed to connect to RabbitMQ", e);
         }
+        
+        onPut(Service.class, (service, serviceId) -> sendUpdateEvent("create", service.name()));
 	}
 }
