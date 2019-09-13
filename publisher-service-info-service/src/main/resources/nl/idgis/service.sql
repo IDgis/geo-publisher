4c45db0a438ecd10877a061f678fbf38cff18bf7
Reijer Copier <reijer.copier@idgis.nl>
2019-09-13 15:54:09 +0200

    Add missing staging attributes

--- publisher-service-info-service/src/main/resources/nl/idgis/service.sql
+++ publisher-service-info-service/src/main/resources/nl/idgis/service.sql
@@ -1,6 +1,7 @@
 with recursive service_layer_structure as (
 	select
 		gl.identification service_id,
+		s.id internal_service_id,
 		null::int parent_layer_id,
 		s.generic_layer_id child_layer_id,
 		null::int style_id,
@@ -11,6 +12,7 @@ with recursive service_layer_structure as (
 	union all
 	select
 		st.service_id,
+		st.internal_service_id,
 		ls.parent_layer_id,
 		ls.child_layer_id,
 		ls.style_id style_id,
@@ -24,6 +26,42 @@ select
 	sls.service_id,
 	sls.anchestors,
 	jsonb_agg(
+		case
+			when sls.parent_layer_id is null then 
+				jsonb_build_object(
+					'keywords', (
+						select array_agg(sk.keyword)
+						from publisher.service_keyword sk
+						where sk.service_id = sls.internal_service_id
+					))
+				|| (
+					select jsonb_build_object(
+						'contact', c.contact,
+						'organization', c.organization,
+						'position', c.position,
+						'addressType', c.address_type,
+						'address', c.address,
+						'city', c.city,
+						'state', c.state,
+						'zipcode', c.zipcode,
+						'country', c.country,
+						'telephone', c.telephone,
+						'fax', c.fax,
+						'email', c.email)
+					from publisher.constants c
+					join publisher.service s on s.constants_id = c.id
+					where s.id = sls.internal_service_id)
+			else '{}'
+		end ||
+		case
+			when ll.id is not null then jsonb_build_object(
+				'keywords', (
+					select array_agg(llk.keyword)
+					from publisher.leaf_layer_keyword llk
+					where llk.leaf_layer_id = ll.id
+				))
+			else '{}'
+		end ||
 		case
 			when sdv.type = 'VECTOR' then 
 				case
@@ -56,7 +94,9 @@ select
 			'id', gl.identification,
 			'name', gl.name,
 			'type', coalesce(lower(sdv.type), 'group'),
-			'title', gl.title) order by sls.layer_order) layers
+			'title', gl.title,
+			'abstract', gl.abstract)
+		order by sls.layer_order) layers
 from service_layer_structure sls
 join publisher.generic_layer gl on gl.id = sls.child_layer_id
 left join publisher.leaf_layer ll on ll.generic_layer_id = gl.id
