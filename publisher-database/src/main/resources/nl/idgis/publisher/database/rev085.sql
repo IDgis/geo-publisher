bc1df0b34a4c28a2ff48e7d969475f350c4b0376
Sandro Neumann <sandro.neumann@idgis.nl>
2022-11-04 17:03:06 +0100

    Add closing character to sql line

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev085.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev085.sql
@@ -1,4 +1,4 @@
 
-create index if not exists source_dataset_version_log_source_dataset_version_id_create_time_idx on source_dataset_version_log using btree(source_dataset_version_id, create_time)
+create index if not exists source_dataset_version_log_source_dataset_version_id_create_time_idx on source_dataset_version_log using btree(source_dataset_version_id, create_time);
 
 insert into publisher.version(id) values(85);
