51f3474fb1fbda3b7b7ade19fbd17c35ebe37a40
Sandro Neumann <sandro.neumann@idgis.nl>
2022-11-11 13:06:48 +0100

    Rename index on source_dataset_version_log

--- publisher-database/src/main/resources/nl/idgis/publisher/database/rev085.sql
+++ publisher-database/src/main/resources/nl/idgis/publisher/database/rev085.sql
@@ -1,4 +1,4 @@
 
-create index if not exists source_dataset_version_log_source_dataset_version_id_create_time_idx on publisher.source_dataset_version_log(source_dataset_version_id, create_time);
+create index if not exists source_dataset_version_log_sdv_id_create_time_idx on publisher.source_dataset_version_log(source_dataset_version_id, create_time);
 
 insert into publisher.version(id) values(85);
